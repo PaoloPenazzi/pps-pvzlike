1e100212fa9d3ff523d0bb65dcb0968dcb403661
paolo <paolo.penazzi@studio.unibo.it>
2022-09-08 17:50:22 +0200

    Add fire rate field to the turret model. Change the turret's health field to hp.

--- src/main/scala/Turret.scala
+++ src/main/scala/Turret.scala
@@ -1,13 +1,15 @@
 trait Turret:
    def position: (Int, Int)
-   def life: Int
+   def hp: Int
+   def fireRate: Int
 
 object Turret:
   def apply(x: Int, y: Int): Turret = new BasicTurret(x, y)
   
   private class BasicTurret(x: Int, y: Int) extends Turret:
     override def position: (Int, Int) = (x,y)
-    override def life: Int = 100
+    override def hp: Int = 100
+    override def fireRate: Int = 1
 
   
 
--- src/test/scala/TurretTest.scala
+++ src/test/scala/TurretTest.scala
@@ -5,6 +5,6 @@ import Turret.*
 class TurretTest extends AnyFunSuite:
   test("An new turret should have 100 hp and be placed in the correct position") {
     val turret = Turret(50, 50)
-    assert(turret.life == 100)
+    assert(turret.hp == 100)
     assert(turret.position == (50, 50))
   }
