494e6a3773419187c4ff4c4e39c092bb5ea59705
paolo <paolo.penazzi@studio.unibo.it>
2022-09-09 15:32:20 +0200

    Create default values for entity attributes

--- src/main/scala/Turret.scala
+++ src/main/scala/Turret.scala
@@ -1,27 +1,28 @@
+import DefaultTowerValues.*
+
 trait Entity:
   def boundary: (Int, Int)
   def position: (Int, Int)
 
 trait AttackingEntity extends Entity:
-  def healthPoints: Int
-  def fireRate: Double
+  def hp: Int = healthPoints(this)
+  def fireRate: Double = fireRates(this)
 
 trait MovingEntity extends Entity:
   def velocity: Double
 
 trait Turret extends Entity with AttackingEntity:
   def bullet: Bullet
-  def cost: Int
+  def cost: Int = costs(this)
 
 trait Bullet extends MovingEntity:
-  def damage: Int
+  def damage: Int = damages(this)
 
 /**
  * Basic bullet shot by a plant.
  * @param position The initial position of the bullet.
  */
 class Seed(override val position: (Int, Int)) extends Bullet:
-  override def damage: Int = 25
   override def velocity: Double = 5.0
   override def boundary: (Int, Int) = (2, 2)
 
@@ -30,11 +31,27 @@ class Seed(override val position: (Int, Int)) extends Bullet:
  * @param position The position in which the plant is placed by the player.
  */
 class Plant(override val position: (Int, Int)) extends Turret:
-  override def cost: Int = 100
-  override def fireRate: Double = 1.0
-  override def healthPoints: Int = 100
   override def boundary: (Int, Int) = (10, 10)
   override def bullet: Seed = new Seed(position)
+
+object DefaultTowerValues:
+  val healthPoints: AttackingEntity => Int =
+    case _: Plant => 100
+    // case _: Zombie => 100
+    case _ => 0
+
+  val fireRates: AttackingEntity => Double =
+    case _: Plant => 1.0
+    // case _: Zombie => 0.8
+    case _ => 0
+
+  val costs: Turret => Int =
+    case _: Plant => 100
+    case _        => 0
+
+  val damages: Bullet => Int =
+    case _: Seed => 25
+    case _       => 0
 
 
 
