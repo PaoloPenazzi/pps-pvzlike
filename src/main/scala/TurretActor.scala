f903064df7acd3f587c71bb7712c290f0e2eaefc
paolo <paolo.penazzi@studio.unibo.it>
2022-09-09 13:17:58 +0200

    Remove the concept of the wave starting/ending according to what we said during the daily meeting. Turrets will have a sighting rage and shoot on sight. [skip ci]

--- src/main/scala/TurretActor.scala
+++ src/main/scala/TurretActor.scala
@@ -1,40 +1,13 @@
 import akka.actor.typed.*
 import akka.actor.typed.scaladsl.Behaviors
 
-import scala.concurrent.duration.DurationInt
-
 trait TurretMessages
-case class waveStarted() extends TurretMessages
-case class waveFinished() extends TurretMessages
-case class shoot() extends TurretMessages
+case class Update() extends TurretMessages
 
 object TurretActor:
   def apply(turret: Turret): Behavior[TurretMessages] =
-    waitingForWave(turret)
-
-
-  def waitingForWave(turret: Turret): Behavior[TurretMessages] =
-    Behaviors.withTimers(timer => {
-      Behaviors.receive { (context, message) =>
-        message match
-          case waveStarted() =>
-            timer.startSingleTimer(shoot(), 2.seconds)
-            waveIncoming(turret)
-      }
-    })
-
-  def waveIncoming(turret: Turret): Behavior[TurretMessages] =
-    print("changed behaviour")
-    Behaviors.withTimers(timer => {
-      Behaviors.receive { (context, message) =>
-        message match
-          case shoot() =>
-            print("shoot")
-            turret.isShooting = true
-            timer.startSingleTimer(shoot(), turret.fireRate.seconds)
-            Behaviors.same
-          case waveFinished() =>
-            turret.isShooting = false
-            waitingForWave(turret)
-      }
-    })
+    Behaviors.receive { (context, message) =>
+      message match
+        case _ =>
+          Behaviors.same
+    }
