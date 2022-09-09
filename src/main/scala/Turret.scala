6937fbc066a67b0079eaa15102793552e651e260
paolo <paolo.penazzi@studio.unibo.it>
2022-09-09 13:48:42 +0200

    Refactor turret model class. Split the class into different traits. [skip ci]

--- src/main/scala/Turret.scala
+++ src/main/scala/Turret.scala
@@ -1,19 +1,18 @@
-trait Turret:
-  var isShooting: Boolean
+trait Entity:
+  def boundary: (Int, Int)
   def position: (Int, Int)
-  def hp: Int
+  def healthPoints: Int
+
+trait AttackingEntity extends Entity:
   def fireRate: Int
 
-object Turret:
-  def apply(x: Int, y: Int): Turret = new BasicTurret(x, y)
-  
-  private class BasicTurret(x: Int, y: Int) extends Turret:
-    var isShooting = false
-    override def position: (Int, Int) = (x,y)
-    override def hp: Int = 100
-    override def fireRate: Int = 1
+trait MovingEntity extends Entity:
+  def velocity: Double
 
-  
+trait Turret extends Entity with AttackingEntity with MovingEntity:
+  def cost: Int
+
+
 
 
 
