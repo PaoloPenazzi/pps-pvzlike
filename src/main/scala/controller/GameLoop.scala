2354c322c8ed578df0abf92d423d09ab37df7e1a
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-09-26 16:01:29 +0200

    Create the backbone for the gameloop actor

---
+++ src/main/scala/controller/GameLoop.scala
@@ -0,0 +1,40 @@
+package controller
+
+import akka.actor.typed.*
+import akka.actor.typed.scaladsl.*
+import akka.actor.typed.scaladsl.adapter.*
+
+import scala.concurrent.duration.FiniteDuration
+
+object GameLoop:
+  object GameLoopCommands:
+    sealed trait GameLoopCommand
+
+    case class Update() extends GameLoopCommand
+
+    case class Start() extends GameLoopCommand
+
+    case class Stop() extends GameLoopCommand
+
+  object GameLoopActor:
+
+    import GameLoopCommands.*
+
+    def apply(): Behavior[GameLoopCommand] =
+      Behaviors.setup(ctx => {
+        Behaviors.withTimers { timer => GameLoopActor(ctx, timer).standardBehavior() }
+      })
+
+    case class GameLoopActor(ctx: ActorContext[GameLoopCommand], timer: TimerScheduler[GameLoopCommand]):
+      def standardBehavior(): Behavior[GameLoopCommand] = Behaviors.receiveMessage(msg => {
+        msg match
+          case Update() =>
+            // update model
+            // update view
+            // find the just time update
+            timer.startSingleTimer(Update(), FiniteDuration(10, "second"))
+            Behaviors.same
+          case Start() => ???
+          case Stop() => ???
+      })
+
