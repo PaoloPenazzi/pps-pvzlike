5c16e0953e179360968e6fd3ebd594eca0d88a79
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-09-27 14:08:37 +0200

    feat: add NewTurretAdded message which allows to add new turret at the game

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -5,14 +5,21 @@ import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import controller.GameLoop.GameLoopActor
 import controller.GameLoop.GameLoopCommands.GameLoopCommand
 import GameLoop.GameLoopCommands.*
+import model.entities.Turrets.Turret
 
-
+/*
+* Essentially the Controller is the controller of all games so for each important phase of a match it will something:
+* - Start the game
+* - Start next round
+* - Handle all response from model/view
+*/
 object Controller:
 
   object ControllerCommands:
     sealed trait ControllerCommand extends Command
     case class StartGame() extends ControllerCommand
     case class FinishGame() extends ControllerCommand
+    case class NewTurretAdded[T<:Turret](turret: T) extends ControllerCommand
 
   object ControllerActor:
 
@@ -26,9 +33,11 @@ object Controller:
         msg match
           case StartGame() =>
             // we will put in gameLoop object the model and the view
+            // lift up game field
             val gameLoop = ctx.spawnAnonymous(GameLoopActor())
             gameLoop ! Start()
             Behaviors.same
+          case NewTurretAdded(turret) => ???
           case FinishGame() => ???
       })
 
