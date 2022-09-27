e3c211b9ace8bd2ce8e7cdf2e0e80a95dbc2b61c
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-09-27 11:21:57 +0200

    Try to test gameloop spawn with few results

--- src/test/scala/controller/ControllerTest.scala
+++ src/test/scala/controller/ControllerTest.scala
@@ -1,16 +1,37 @@
 package controller
 
 import akka.actor.testkit.typed.Effect
-import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, BehaviorTestKit, TestInbox}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
-import org.scalatest.funsuite.AnyFunSuite
+import controller.Controller.ControllerActor
+import controller.Controller.ControllerCommands.StartGame
+import controller.GameLoop.GameLoopActor
+import controller.GameLoop.GameLoopCommands.{GameLoopCommand, Start}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.must.Matchers.must
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class ControllerTest extends AnyFunSuite:
-  import controller.Controller.*
+class ControllerTest extends AnyWordSpec with BeforeAndAfterAll with Matchers :
+  val testKit = ActorTestKit()
+  val controller = BehaviorTestKit(ControllerActor())
 
-  test("Test Controller Actor Spawning") {
-    val controllerActorTest = BehaviorTestKit(ControllerActor())
-    controllerActorTest.isAlive
+
+  override def afterAll(): Unit = testKit.shutdownTestKit()
+
+  "The Controller Actor" when {
+    "created" should {
+      "be alive" in {
+        controller.isAlive must be(true)
+      }
+
+      /*      "spawn a new game loop" in {
+        controller run StartGame()
+        val childBox = controller.childInbox[GameLoopCommand]("gameloop")
+        childBox.expectMessage(Start())
+        controller expectEffect Effect.Spawned(GameLoopActor(), "gameloop")
+        }
+    }*/
+    }
   }
-
