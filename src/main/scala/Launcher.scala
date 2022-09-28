a37cb240bd1badf3e3205e10cd40996ee50c478b
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-09-28 17:53:56 +0200

    feat: first implementation of launcher class

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -1,20 +1,37 @@
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler, Terminated}
 import akka.util.Timeout
 import controller.Command
 import controller.GameController.GameControllerActor
-import controller.GameController.GameControllerCommands.{GameControllerCommand, StartGame, FinishGame}
+import controller.GameController.GameControllerCommands.{FinishGame, GameControllerCommand, NewGame, StartGame}
+import controller.GameLoop.GameLoopActor
+import controller.WaveController.WaveControllerActor
 
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Success
 
+
 object Launcher {
 
+  // to rebuild...
   @main
-  def main() =
-    val controller: ActorSystem[GameControllerCommand] = ActorSystem(GameControllerActor(), name = "launcher")
-    controller ! StartGame()
+  def main(): Unit =
+    val system = ActorSystem(
+      Behaviors.setup[Any] { ctx =>
+      val gameLoop = ctx.spawnAnonymous(GameLoopActor())
+      val waveController = ctx.spawnAnonymous(WaveControllerActor())
+      Behaviors.receiveMessage[Any] { _ =>
+        val gameController = ctx.spawnAnonymous(GameControllerActor(gameLoop, waveController))
+        ctx.watch(gameController)
+        gameController ! NewGame()
+        Behaviors.same
+      }.receiveSignal { case (_, _ @Terminated(_)) =>
+        System.out.println("PVZ terminated. Shutting down")
+        Behaviors.stopped
+      }
+    }, "main")
+    system ! StartGame()
 
 }
