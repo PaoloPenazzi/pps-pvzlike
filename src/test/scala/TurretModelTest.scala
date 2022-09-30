c7578361c6f77a53d9e441f7ad41fa96164e01c8
paolo <paolo.penazzi@studio.unibo.it>
2022-09-30 13:50:20 +0200

    test: add test to turret model
    the turret shouldn't attack an enemy that is not in the same lane

--- src/test/scala/TurretModelTest.scala
+++ src/test/scala/TurretModelTest.scala
@@ -4,9 +4,17 @@ import matchers.*
 import model.entities.{Plant, Zombie, Enemy, Turret}
 
 class TurretModelTest extends AnyFlatSpec with should.Matchers:
-  "A turret" should "see a zombie in range" in {
-    val turret: Turret = Plant(50, 1)
-    val zombie: Enemy = Zombie()
-    zombie.position = (100, 1)
-    turret canAttack zombie shouldBe true
+  "A turret" should "attack a zombie that is in range" in {
+      val turret: Turret = Plant(50, 1)
+      val zombie: Enemy = Zombie()
+      zombie.position = (100, 1)
+      turret canAttack zombie shouldBe true
   }
+  "A turret" should "not attack a zombie in another lane" in {
+      val turret: Turret = Plant(50, 1)
+      val zombie: Enemy = Zombie()
+      zombie.position = (100, 2)
+      turret canAttack zombie shouldBe false
+  }
+
+
