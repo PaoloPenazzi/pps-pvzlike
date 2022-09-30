db98bc44edbd59505968801310262e4e2d3f574a
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-09-30 18:55:34 +0200

    feat: create functions to detect collision, interestest, update and wave generation

--- src/main/scala/controller/GameLoopActor.scala
+++ src/main/scala/controller/GameLoopActor.scala
@@ -39,9 +39,9 @@ object GameLoopActor:
     case class Stop() extends GameLoopCommand
 
   // TODO from here, make it better...
-  var enemiesWave = mutable.HashMap[ActorRef[Enemy], Enemy]()
-  var bullets = mutable.HashMap[ActorRef[Enemy], Enemy]()
-  var entities = mutable.HashMap[ActorRef[Enemy], Enemy]()
+  var enemiesWave: Seq[(ActorRef[Enemy], Enemy)] = List[(ActorRef[Enemy], Enemy)]()
+  var bullets: Seq[(ActorRef[Bullet], Bullet)] = List[(ActorRef[Bullet], Bullet)]()
+  var entities: Seq[(ActorRef[Entity], Entity)] = List[(ActorRef[Entity], Entity)]()
 
   def apply(): Behavior[Command] =
     Behaviors.setup { _ => Behaviors.withTimers { timer => GameLoopActor(timer).standardBehavior() } }
@@ -56,20 +56,16 @@ object GameLoopActor:
           startTimer(timer)
           Behaviors.same
         case Update() =>
-          // update model
-          // update view
-          // find the correct time update
-          //wave.foreach(enemy ! update)
-          detectCollision()
-          detectInterest()
-          entities.foreach(_)
-          timer.startSingleTimer(Update(), FiniteDuration(10, "second"))
+          // todo check if the wave is end
+          detectCollision
+          updateAll(detectInterest)
+          startTimer(timer)
           Behaviors.same
         case Start(wave) =>
           // model ! start
           // view ! start
           // enemiesWave = Some(wave)
-          timer.startSingleTimer(Update(), FiniteDuration(10, "second"))
+          startTimer(timer)
           Behaviors.same
         case EntityUpdate(entity) =>
           // pass the model to view
@@ -91,11 +87,33 @@ object GameLoopActor:
     def startTimer(timer: TimerScheduler[Command]) = timer.startSingleTimer(Update(), FiniteDuration(10, "second"))
 
     def createWave(ctx: ActorContext[Command]) = ???
-      // WaveSupervisor.generateWave(2).map( e => (ctx.spawnAnonymous(EnemyActor(enemy)), e)).foreach(t => enemiesWave + t)
+      // genera una wave di zombie poi la lista di zombie creata verrà mappata in un'altra lista fatta di tuple (zombieRef, zombie)
+      // e alla fine gli zombie verranno aggiunti alla lista enemiesWave lista che contiene tutti gli zombie
+      // WaveSupervisor.generateWave(2).map( e => (ctx.spawnAnonymous(EnemyActor(enemy)), e)).foreach(t => enemiesWave = enemiesWave + t)
 
-    def detectCollision() = ???
+    def detectCollision = ???
+      // todo provarlo a fare con for-yield o pimp-my-library
+      // controlla per ogni bullet nel gioco se questo ha colpito qualche entità (che non sia lui). Se il bullet ha colpito qualcuno (quindi
+      // la lista entitiesColl non è vuota) allora mando un messaggio di collisione ad ogni entità e al bullet in questione
+       /*bullets.foreach(b =>
+         val entitiesColl = entities.filter(e => (b._1 != e._1) && b._2.collideWith(e._2))
+         if entitiesColl.nonEmpty then entities.foreach(e => {e._1 ! CollisionWith(b._2); b._1 ! CollisionWith(e._2)}))*/
 
-    def detectInterest() = ???
+
+    def detectInterest: Seq[(ActorRef[Entity], Seq[Entity])] =  ???
+    // crea una lista contenente (riferimento all entità, lista di entità a cui è interessato)
+      /*for
+        e1 <- entities
+      yield
+        (e1._1, for
+          e2 <- entities
+          if e1._1 != e2._1
+          if e1._2.filter(e2._2)
+        yield e2._2)*/
+
+    def updateAll(interestForAll: Seq[(ActorRef[Entity], Seq[Entity])]) = ???
+      // mando un messaggio di update ad ogni entità e gli allego le entità a cui è interessato
+      // interestForAll.foreach(e => e._1 ! UpdateModel(e._2.map( x => x._2)))
 
 
 
