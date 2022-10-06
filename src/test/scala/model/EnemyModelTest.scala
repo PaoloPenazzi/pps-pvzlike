de538bcb614b9005b7cfad33b7ad2c2a44d5ecb0
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-06 17:14:20 +0200

    test: refactor enemies attack test

--- src/test/scala/model/EnemyModelTest.scala
+++ src/test/scala/model/EnemyModelTest.scala
@@ -1,19 +1,20 @@
 package model
 
-import model.entities.WorldSpace.LanesLength
 import model.entities.{Enemy, Entity, Plant, Turret, WorldSpace, Zombie}
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should
+import org.scalatest.*
+import org.scalatest.flatspec.*
+import org.scalatest.matchers.*
+import WorldSpace.{LanesLength, given}
 
 class EnemyModelTest extends AnyFlatSpec with should.Matchers:
   "A zombie" should "attack a turrets that is in range" in {
-    val turret: Turret = Plant(50, 1)
-    val zombie: Enemy =  Zombie(100, 1)
+    val turret: Turret = Plant(1, LanesLength / 2)
+    val zombie: Enemy =  Zombie(1, (LanesLength / 2 + 5) )
     zombie canAttack turret shouldBe true
   }
-  "A turret" should "not attack a zombie in another lane" in {
-    val turret: Turret = Plant(50, 1)
-    val zombie: Enemy = Zombie(100, 2)
+  "A zombie" should "not attack a turret in another lane" in {
+    val turret: Turret = Plant(1, LanesLength / 2)
+    val zombie: Enemy = Zombie(2, (LanesLength / 2 + 5))
     zombie canAttack turret shouldBe false
   }
 
