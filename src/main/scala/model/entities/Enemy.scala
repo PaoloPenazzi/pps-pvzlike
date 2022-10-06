2be1b8b1a26d50e199420f710e8d09f0e6ceca64
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-06 17:56:29 +0200

    feat: enemy filter the interesting entities

--- src/main/scala/model/entities/Enemy.scala
+++ src/main/scala/model/entities/Enemy.scala
@@ -13,10 +13,15 @@ trait Enemy extends MovingEntity with AttackingEntity:
   def canAttack(turret: Turret): Boolean =
     isOnMyPath(turret) && isInRange(turret)
 
-  def isOnMyPath(turret: Turret): Boolean
+  protected def isOnMyPath(turret: Turret): Boolean
 
-  def isInRange(turret: Turret): Boolean
+  protected def isInRange(turret: Turret): Boolean
 
+  /**
+   * filters to keep only all game entities present on the same lane
+   * @return true if it is of his own interest
+   */
+  override def filter: Entity => Boolean
 
 /**
  * Basic Enemy.
@@ -27,6 +32,9 @@ class Zombie(override val position: Position) extends Enemy:
     turret.position.y == position.y
   override def isInRange(turret: Turret): Boolean =
     position.x - turret.position.x.toInt <= range
+  override def filter: Entity => Boolean =
+    case turret: Turret => turret.position.y == position.y
+    case _ => false
   override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Enemy =
     Zombie(updatePosition(elapsedTime))
 
