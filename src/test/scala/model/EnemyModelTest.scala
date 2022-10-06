0b233d93c9ea5fce848955bf07668a88bb22711a
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-06 17:36:11 +0200

    test: implemented test to verify that an enemy has the reference only of the entities of his interest

--- src/test/scala/model/EnemyModelTest.scala
+++ src/test/scala/model/EnemyModelTest.scala
@@ -17,5 +17,15 @@ class EnemyModelTest extends AnyFlatSpec with should.Matchers:
     val zombie: Enemy = Zombie(2, (LanesLength / 2 + 5))
     zombie canAttack turret shouldBe false
   }
+  "A enemy" should "filter the interesting entities" in {
+    val basicZombie: Enemy = Zombie(1, LanesLength / 2)
+    val firstTurretInSecondLane: Turret = Plant(2, LanesLength)
+    val secondTurretInSecondLane: Turret = Plant(2, LanesLength * 0.75)
+    val firstZombieFirstLane: Enemy = Zombie(1, LanesLength)
+    val thirdZombieInSecondLane: Enemy = Zombie(2, LanesLength)
+    val entities: List[Entity] = List(firstTurretInSecondLane, secondTurretInSecondLane,
+      firstZombieFirstLane, thirdZombieInSecondLane)
+    assert(entities.filter(basicZombie.filter) == List(basicZombie))
+  }
 
 
