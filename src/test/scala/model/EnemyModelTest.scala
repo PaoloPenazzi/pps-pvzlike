d60e862d9a86298fa4cf4e398c4ebb8101c6a2d6
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-06 15:54:24 +0200

    Write a first test for enemies attack

--- src/test/scala/model/EnemyModelTest.scala
+++ src/test/scala/model/EnemyModelTest.scala
@@ -1,19 +1,17 @@
 package model
 
-import model.entities.{Enemy, Plant, Turret}
+import model.entities.{Enemy, Zombie, Plant, Turret}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class EnemyModelTest extends AnyFlatSpec with should.Matchers:
   "A zombie" should "attack a turrets that is in range" in {
     val turret: Turret = Plant(50, 1)
-    val zombie: Enemy =  Zombie()
-    zombie.position = (100, 1)
-    turret canAttack zombie shouldBe true
+    val zombie: Enemy =  Zombie(100, 1)
+    zombie canAttack turret shouldBe true
   }
   "A turret" should "not attack a zombie in another lane" in {
     val turret: Turret = Plant(50, 1)
-    val zombie: Enemy = Zombie()
-    zombie.position = (100, 2)
-    turret canAttack zombie shouldBe false
+    val zombie: Enemy = Zombie(100, 2)
+    zombie canAttack turret shouldBe false
   }
