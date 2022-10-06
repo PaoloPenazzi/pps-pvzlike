73976e0e8f7f28f238289dfd59bfaf658ed634e4
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-06 17:17:34 +0200

    feat: implemented a first attack mechanism of the enemy

--- src/main/scala/model/entities/Enemy.scala
+++ src/main/scala/model/entities/Enemy.scala
@@ -4,13 +4,29 @@ import model.entities.WorldSpace.Position
 
 import scala.concurrent.duration.FiniteDuration
 
+/**
+ * Interface of an enemy model
+ */
 trait Enemy extends MovingEntity with AttackingEntity:
   override type UpdatedEntity = Enemy
-  override def velocity: Float = -0.001
+
+  def canAttack(turret: Turret): Boolean =
+    isOnMyPath(turret) && isInRange(turret)
+
+  def isOnMyPath(turret: Turret): Boolean
+
+  def isInRange(turret: Turret): Boolean
+
+
 /**
- * Basic zombie.
+ * Basic Enemy.
  */
 class Zombie(override val position: Position) extends Enemy:
+  override def velocity: Float = -0.001
+  override def isOnMyPath(turret: Turret): Boolean =
+    turret.position.y == position.y
+  override def isInRange(turret: Turret): Boolean =
+    position.x - turret.position.x.toInt <= range
   override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Enemy =
     Zombie(updatePosition(elapsedTime))
 
