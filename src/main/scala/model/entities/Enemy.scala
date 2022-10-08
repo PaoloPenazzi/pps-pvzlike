7b045e918e821663de70dbf7abeff1c8e2c491e1
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-08 16:00:43 +0200

    refactor: canAttack method of enemy

--- src/main/scala/model/entities/Enemy.scala
+++ src/main/scala/model/entities/Enemy.scala
@@ -10,31 +10,29 @@ import scala.concurrent.duration.FiniteDuration
 trait Enemy extends MovingEntity with AttackingEntity:
   override type UpdatedEntity = Enemy
 
-  def canAttack(turret: Turret): Boolean =
-    isOnMyPath(turret) && isInRange(turret)
-
-  protected def isOnMyPath(turret: Turret): Boolean
-
-  protected def isInRange(turret: Turret): Boolean
+  def canAttack(turret: Turret): Boolean
 
   /**
    * filters to keep only all game entities present on the same lane
    * @return true if it is of his own interest
    */
-  override def filter: Entity => Boolean
+  def filter: Entity => Boolean
 
 /**
  * Basic Enemy.
  */
 class Zombie(override val position: Position) extends Enemy:
+  assert(position != null)
   override def velocity: Float = -0.001
-  override def isOnMyPath(turret: Turret): Boolean =
-    turret.position.y == position.y
-  override def isInRange(turret: Turret): Boolean =
-    position.x - turret.position.x.toInt <= range
+
+  override def canAttack(turret: Turret): Boolean =
+    filter(turret) && position.x - turret.position.x.toInt <= range
+
   override def filter: Entity => Boolean =
     case turret: Turret => turret.position.y == position.y
     case _ => false
+
   override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Enemy =
     Zombie(updatePosition(elapsedTime))
+
 
