057cbb82605d0bebcfb0419235d8a4867a8a5bce
paolo <paolo.penazzi@studio.unibo.it>
2022-10-09 18:03:46 +0200

    test: add wave enemies number test

--- src/test/scala/WaveTest.scala
+++ src/test/scala/WaveTest.scala
@@ -10,3 +10,12 @@ class WaveTest extends AnyFlatSpec with should.Matchers:
     val testWave: Wave = generator.generateNextWave
     assertResult(3)(testWave.waveNumber)
   }
+
+  "The fourth wave" should "have 7 zombies" in {
+    val generator: WaveGenerator = Generator()
+    generator.generateNextWave
+    generator.generateNextWave
+    generator.generateNextWave
+    val testWave: Wave = generator.generateNextWave
+    assertResult(7)(testWave.enemies.size)
+  }
