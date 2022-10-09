8e10a941bfb2066692ba93c7126ed7ba4bda39c5
paolo <paolo.penazzi@studio.unibo.it>
2022-10-09 18:02:25 +0200

    feat: change the wave generation to increment enemies number each wave
    the number of enemy in wave n is: (n*2)-1

--- src/main/scala/controller/WaveManager.scala
+++ src/main/scala/controller/WaveManager.scala
@@ -4,10 +4,12 @@ import controller.Generator.WaveImpl
 import model.entities.WorldSpace.given
 import model.entities.{Enemy, Zombie}
 
+import scala.annotation.tailrec
+
 trait Wave:
   def waveNumber: Int
   def enemies: List[Enemy]
-  
+
 trait WaveGenerator:
   def generateNextWave: Wave
 
@@ -18,10 +20,14 @@ object Generator:
 
   private case class WaveGeneratorImpl() extends WaveGenerator:
     private var waveNumber: Int = 0
-    
+
     override def generateNextWave: Wave =
       waveNumber = waveNumber + 1
-      val newEnemies = List.fill(2)(Zombie((1, 100)))
+      val newEnemies = createEnemyList(2 * waveNumber - 1)(List.empty[Enemy])
       WaveImpl(waveNumber, newEnemies)
-      
 
+    @tailrec
+    private def createEnemyList(n: Int)(l: List[Enemy]): List[Enemy] =
+      n match
+        case 0 => l
+        case _ => createEnemyList(n - 1)(l = l :+ new Zombie(1,100))
