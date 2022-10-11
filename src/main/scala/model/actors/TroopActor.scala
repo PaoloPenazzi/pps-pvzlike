b13e52b889211c3794d1171823f7224f2e2100ad
paolo <paolo.penazzi@studio.unibo.it>
2022-10-11 15:55:12 +0200

    feat: create a basic implementation of a troop actor

--- src/main/scala/model/actors/TroopActor.scala
+++ src/main/scala/model/actors/TroopActor.scala
@@ -1,5 +1,25 @@
 package model.actors
 
-class TroopActor {
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import controller.GameLoopActor.GameLoopCommands.EntitySpawned
+import model.entities.*
 
-}
+object TroopActor:
+  def apply(troop: Troop): Behavior[ModelMessage] =
+    standardBehaviour(troop)
+
+  def standardBehaviour(troop: Troop): Behavior[ModelMessage] =
+    Behaviors.withTimers(timer => {
+      Behaviors.receive((ctx, msg) => {
+        msg match
+          case Update(_, entities, replyTo) => ???
+
+          case Shoot(replyTo) => ???
+
+          case Collision(entity, replyTo) => ???
+
+          case _ => Behaviors.same
+      })
+    })
+
