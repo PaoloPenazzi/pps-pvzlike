1a1399f0536d9f82cbf0a4a33f25b7954a74877b
paolo <paolo.penazzi@studio.unibo.it>
2022-10-11 16:49:40 +0200

    feat: implement update message handler

--- src/main/scala/model/actors/TroopActor.scala
+++ src/main/scala/model/actors/TroopActor.scala
@@ -4,6 +4,7 @@ import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import controller.GameLoopActor.GameLoopCommands.{EntitySpawned, EntityUpdated}
 import model.entities.*
+import concurrent.duration.DurationInt
 
 object TroopActor:
   def apply(troop: Troop): Behavior[ModelMessage] =
@@ -16,6 +17,15 @@ object TroopActor:
           case Update(elapsedTime, entities, replyTo) =>
             val entityUpdated: Entity = troop.update(elapsedTime, entities)
             replyTo ! EntityUpdated(ctx.self, entityUpdated)
+            troop match
+              case _: AttackingEntity =>
+                entities.find(enemy => troop.asInstanceOf[AttackingEntity] canAttack enemy) match
+                  case Some(_) =>
+                    if !timer.isTimerActive("Shooting")
+                    then
+                      timer.startSingleTimer("Shooting", Shoot(replyTo), troop.asInstanceOf[AttackingEntity].fireRate.seconds)
+                  case _ =>
+              case _ =>
             standardBehaviour(entityUpdated.asInstanceOf[Troop])
 
           case Shoot(replyTo) => ???
