1f61a5207fefc0c6bdae2b5becda3557151876f2
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-14 17:42:25 +0200

    test: first integration test from gameloop with other actors

--- src/test/scala/controller/integration/GameLoopIntegrationTest.scala
+++ src/test/scala/controller/integration/GameLoopIntegrationTest.scala
@@ -1,7 +1,38 @@
 package controller.integration
 
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
+import controller.{Command, ViewMessage}
+import controller.GameLoopActor.GameLoopActor
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class GameLoopIntegrationTest extends AnyWordSpec with Matchers :
-  ???
+
+  val view: TestInbox[ViewMessage] = TestInbox[ViewMessage]()
+  val gameLoop: TestInbox[Command] = TestInbox[Command]()
+  val waveController: TestInbox[Command] = TestInbox[Command]()
+
+  "GameController" when {
+    "communicate correctly" should {
+
+      "interact with actors when updating" in {
+        val gameLoop: BehaviorTestKit[Command] = BehaviorTestKit(GameLoopActor(view.ref, List()).standardBehavior)
+      }
+
+      "interact with bullet actors when find a collisions" in {
+
+      }
+
+      "interact with WaveController" in {
+        // gameController run NewGame()
+        // awful, but only for the moment...first wave and two enemies
+        // waveController expectMessage GenerateWave(1, 2, gameController.ref)
+      }
+
+      "interact with GameLoop" in {
+//        gameController run NewEnemiesWave(List())
+//        gameController run StartGame()
+//        gameLoop expectMessage Start(List())
+      }
+    }
+  }
