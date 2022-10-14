00a6b6eedb089a23c692f491911afdebd89896e8
paolo <paolo.penazzi@studio.unibo.it>
2022-10-14 09:19:11 +0200

    test: add test to check turret life after a collision

--- src/test/scala/model/TurretModelTest.scala
+++ src/test/scala/model/TurretModelTest.scala
@@ -12,6 +12,7 @@ class TurretModelTest extends AnyFlatSpec with should.Matchers:
   val dummyTurret1: Turret = Plant(2, LanesLength)()
   val dummyZombie1: Enemy = Zombie((1, LanesLength))
   val dummyZombie2: Enemy = Zombie((2, LanesLength))
+  val dummyBullet: Bullet = Seed(0,0)
 
   "A turret" should "attack a zombie that is in range" in {
       testTurret canAttack dummyZombie1 shouldBe true
@@ -20,4 +21,8 @@ class TurretModelTest extends AnyFlatSpec with should.Matchers:
     val entities: List[Entity] = List(dummyTurret1, dummyZombie1, dummyZombie2)
     val entitiesFiltered = entities.filter(testTurret.isInterestedIn)
     assertResult(entitiesFiltered)(List(dummyZombie1))
+  }
+  "A turret" should "lose HPs after getting hitted" in {
+    val updatedTurret = testTurret collideWith dummyBullet
+    assert(updatedTurret.get.life < testTurret.life)
   }
