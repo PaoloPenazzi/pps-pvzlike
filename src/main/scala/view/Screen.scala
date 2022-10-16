07b66f2cc991bd59ea246c4422361aeaccc4c225
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-16 17:50:25 +0200

    chore: implemented method to create buttons from images of game window

--- src/main/scala/view/Screen.scala
+++ src/main/scala/view/Screen.scala
@@ -1,6 +1,6 @@
 package view
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.{SpriteBatch, TextureRegion}
 import com.badlogic.gdx.graphics.{GL20, OrthographicCamera, Texture}
 import com.badlogic.gdx.math.{Vector2, Vector3}
 import com.badlogic.gdx.physics.box2d.{Box2DDebugRenderer, World}
@@ -11,6 +11,8 @@ import model.entities.*
 import View.EntityRenderer
 import Sprites.*
 import ViewportSpace.*
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 
 object Screen:
   val Framerate: Float = 60
@@ -23,30 +25,40 @@ class Screen(private val viewport: Viewport) extends ScreenAdapter with EntityRe
   private var entities: List[Entity] = List.empty
   private lazy val batch: SpriteBatch = SpriteBatch()
 
+
   override def render(delta: Float): Unit =
     world.step(1 / Screen.Framerate, 6, 2)
-
     batch.setProjectionMatrix(camera.combined)
-
     Gdx.gl.glClearColor(0, 0, 0, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
     val background: Texture = new Texture(Gdx.files.classpath("assets/background/day.png"))
 
     val gamingWindowPlants: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/pea-shooter-button.png"))
+    val peaShooterButton = createButtonFromImage(gamingWindowPlants)
     val gamingWindowSunFlowers: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/sunflower-button.png"))
-    val gamingWindowSunPotato: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/wallnut-button.png"))
+    val sunFlowerButton = createButtonFromImage(gamingWindowSunFlowers)
+    val gamingWindowWallnut: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/wallnut-button.png"))
+    val wallnutButton = createButtonFromImage(gamingWindowWallnut)
     val gamingWindowNumberOfSun: Texture = Texture(Gdx.files.classpath("assets/gameWindow/numberOfSun.png"))
 
     batch.begin()
     batch.draw(background, -3, projectY(0), 25, 7.5f)
     batch.draw(gamingWindowPlants, 0, 7.5f, 1.5f, 1.5f)
     batch.draw(gamingWindowSunFlowers, 1.5f, 7.5f, 1.5f, 1.5f)
-    batch.draw(gamingWindowSunPotato, 3, 7.5f, 1.5f, 1.5f)
+    batch.draw(gamingWindowWallnut, 3, 7.5f, 1.5f, 1.5f)
     batch.draw(gamingWindowNumberOfSun, 12, 7.5f, 4, 1.5f)
     entities.foreach(e => batch.draw(texture(e), projectX(e.position.x), projectY(e.position.y), width(e), height(e)))
 
     batch.end()
+
+  private def createButtonFromImage(texture: Texture): ImageButton =
+    val textureRegion = new TextureRegion(texture)
+    val regionDrawable = new TextureRegionDrawable(textureRegion)
+    val button = new ImageButton(regionDrawable)
+    button
+
+
 
   override def resize(width: Int, height: Int): Unit =
     viewport.update(width, height)
