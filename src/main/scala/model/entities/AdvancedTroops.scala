15d9c171fd1dac98be99e4efa68b88b9a3fcc432
paolo <paolo.penazzi@studio.unibo.it>
2022-10-18 11:27:55 +0200

    feat: create advanced entity and troop traits

--- src/main/scala/model/entities/AdvancedTroops.scala
+++ src/main/scala/model/entities/AdvancedTroops.scala
@@ -6,3 +6,15 @@ object Troops:
   trait AdvancedBullet
   trait AdvancedPeaBullet extends AdvancedBullet
   trait AdvancedZombieBullet extends AdvancedBullet
+
+  trait AdvancedEntity
+
+  trait AdvancedAttackingAbility:
+    def fireRate: Int
+    def sightRange: Int
+    def withFireRate(rate: Int): AdvancedAttackingAbility
+    def withSightRange(range: Int): AdvancedAttackingAbility
+
+  trait Troop extends AdvancedEntity with AdvancedAttackingAbility:
+    override def withFireRate(rate: Int): Troop
+    override def withSightRange(range: Int): Troop
