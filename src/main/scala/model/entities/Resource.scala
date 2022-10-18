db523e0987bd61148c7d16f6a3f7fab6f0648121
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-18 20:20:58 +0200

    feat: add method for sum and subtract sun

--- src/main/scala/model/entities/Resource.scala
+++ src/main/scala/model/entities/Resource.scala
@@ -1,13 +1,18 @@
 package model.entities
 
+import scala.annotation.targetName
 import scala.concurrent.duration.FiniteDuration
 
 trait Resource[T]:
   def value: T
   def spawnTime: FiniteDuration
 
-case class Sun(override val value: Int,
-               override val spawnTime: FiniteDuration) extends Resource[Int]
+case class Sun(override val value: Int)(override val spawnTime: FiniteDuration = FiniteDuration(5, "seconds")) extends Resource[Int]:
+  @targetName("sum")
+  def -(sun: Sun): Sun = Sun(value - sun.value)()
+
+  @targetName("subtraction")
+  def +(sun: Sun): Sun = Sun(value + sun.value)()
 
 
 
