d5aee98e04dcdbc5fe7c00f19bd6b9e84de08001
paolo <paolo.penazzi@studio.unibo.it>
2022-10-18 16:01:39 +0200

    feat: add given conversion

--- src/main/scala/model/entities/AdvancedTroops.scala
+++ src/main/scala/model/entities/AdvancedTroops.scala
@@ -14,23 +14,23 @@ object Troops:
     def withFireRate(rate: Int): AdvancedAttackingAbility
     def withSightRange(range: Int): AdvancedAttackingAbility
 
-  trait Troop[B <: Bullet] extends AdvancedEntity with AdvancedAttackingAbility:
+  trait AdvancedTroop[B <: AdvancedBullet] extends AdvancedEntity with AdvancedAttackingAbility:
     def bullet: B
-    override def withFireRate(rate: Int): Troop[B]
-    override def withSightRange(range: Int): Troop[B]
+    override def withFireRate(rate: Int): AdvancedTroop[B]
+    override def withSightRange(range: Int): AdvancedTroop[B]
 
-  object TroopBuilders:
-    trait TroopBuilder[B <: Bullet]:
-      def build(b: B): Troop[B]
-    given TroopBuilder[AdvancedPeaBullet] with
-      override def build(bullet: AdvancedPeaBullet): Troop[AdvancedPeaBullet] = Troop[AdvancedPeaBullet]()
-    given TroopBuilder[AdvancedZombieBullet] with
-      override def build(bullet: AdvancedZombieBullet): Troop[AdvancedZombieBullet] = BaseZombie[AdvancedZombieBullet]()
+  trait TroopBuilder[B <: AdvancedBullet]:
+    def build(bullet: B): AdvancedTroop[B]
+  given TroopBuilder[AdvancedPeaBullet] with
+    override def build(bullet: AdvancedPeaBullet): AdvancedTroop[AdvancedPeaBullet] = BasePlant[AdvancedPeaBullet]()
+  given TroopBuilder[AdvancedZombieBullet] with
+    override def build(bullet: AdvancedZombieBullet): AdvancedTroop[AdvancedZombieBullet] = BaseZombie[AdvancedZombieBullet]()
 
-  import TroopBuilders.*
-
-  def ofType[B <: Bullet](using troopBuilder: TroopBuilder[Bullet])(bullet: B): Troop[B] =
+  def ofType[B <: AdvancedBullet](using troopBuilder: TroopBuilder[B])(bullet: B): AdvancedTroop[B] =
     troopBuilder.build(bullet)
 
-  case class BasePlant[B <: Bullet]() extends Troop[Bullet]
-  case class BaseZombie[B <: Bullet]() extends Troop[Bullet]
+  case class BasePlant[B <: AdvancedBullet]() extends AdvancedTroop[AdvancedPeaBullet]
+    
+  case class BaseZombie[B <: AdvancedBullet]() extends AdvancedTroop[AdvancedZombieBullet]
+
+  // Troops ofType Peashooter inPosition (20,2)
