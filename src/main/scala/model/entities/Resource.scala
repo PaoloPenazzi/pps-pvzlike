1d0b7c08bcee9fdf4595de5649c0cdc8b0e730f8
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-19 12:35:38 +0200

    refactor: delete some useless def (sum, sub), because sun model is essentially an int

--- src/main/scala/model/entities/Resource.scala
+++ src/main/scala/model/entities/Resource.scala
@@ -5,14 +5,10 @@ import scala.concurrent.duration.FiniteDuration
 
 trait Resource[T]:
   def value: T
-  def spawnTime: FiniteDuration
-
-case class Sun(override val value: Int)(override val spawnTime: FiniteDuration = FiniteDuration(5, "seconds")) extends Resource[Int]:
-  @targetName("sum")
-  def -(sun: Sun): Sun = Sun(value - sun.value)()
-
-  @targetName("subtraction")
-  def +(sun: Sun): Sun = Sun(value + sun.value)()
+  def duration: FiniteDuration
+  
+case class Sun(override val value: Int = 25,
+               override val duration: FiniteDuration = FiniteDuration(5, "second")) extends Resource[Int]
 
 
 
--- src/test/scala/model/ResourceModelTest.scala
+++ src/test/scala/model/ResourceModelTest.scala
@@ -6,16 +6,8 @@ import org.scalatest.matchers.should.Matchers
 
 class ResourceModelTest extends AnyFlatSpec with Matchers:
 
-  val mockSun: Sun = Sun(30)()
-  val anotherMockSun: Sun = Sun(40)()
-
-  "A sun" should "sum with other sun" in {
-    mockSun + anotherMockSun shouldBe Sun(70)()
-  }
-
-  "A sun" should "subtract with other sun" in {
-    mockSun - anotherMockSun shouldBe Sun(-10)()
-  }
+  val mockSun: Sun = Sun()
+  val anotherMockSun: Sun = Sun()
 
 
 
