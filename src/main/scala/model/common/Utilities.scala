432b78900bb4a37e45097cc7045179f8ca139052
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-19 17:41:12 +0200

    refactor: delete resource file and brought to utilities file all the information about metadatas

--- src/main/scala/model/common/Utilities.scala
+++ src/main/scala/model/common/Utilities.scala
@@ -1,5 +1,7 @@
 package model.common
 
+import model.entities.Turret
+
 import scala.concurrent.duration.FiniteDuration
 
 object Utilities:
@@ -8,3 +10,10 @@ object Utilities:
     case Slow extends Velocity(FiniteDuration(32, "milliseconds"))
     case Normal extends Velocity(FiniteDuration(16, "milliseconds"))
     case Fast extends Velocity(FiniteDuration(8, "milliseconds"))
+    
+  enum Sun(val value: Int):
+    case Small extends Sun(15)
+    case Normal extends Sun(25)
+    case Big extends Sun(50)
+
+  case class MetaData(sun: Int = 0, turrets: Seq[Turret] = List.empty, velocity: Velocity = Velocity.Normal)
