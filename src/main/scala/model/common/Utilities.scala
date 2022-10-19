c90f362ec3d80dcde8cb726075b94b39a55373e2
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-19 15:14:14 +0200

    feat: add Velocity class that define three game's velocities

---
+++ src/main/scala/model/common/Utilities.scala
@@ -0,0 +1,10 @@
+package model.common
+
+import scala.concurrent.duration.FiniteDuration
+
+object Utilities:
+  
+  enum Velocity(val speed: FiniteDuration):
+    case Slow extends Velocity(FiniteDuration(32, "milliseconds"))
+    case Normal extends Velocity(FiniteDuration(16, "milliseconds"))
+    case Fast extends Velocity(FiniteDuration(8, "milliseconds"))
