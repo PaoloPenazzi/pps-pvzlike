23526a641789509ba9553b76edee6dc8c8b9bc92
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-19 15:18:28 +0200

    feat: add a value for each speed velocity

--- src/main/scala/model/common/Utilities.scala
+++ src/main/scala/model/common/Utilities.scala
@@ -3,7 +3,7 @@ package model.common
 import scala.concurrent.duration.FiniteDuration
 
 object Utilities:
-  
+
   enum Velocity(val speed: FiniteDuration):
     case Slow extends Velocity(FiniteDuration(32, "milliseconds"))
     case Normal extends Velocity(FiniteDuration(16, "milliseconds"))
