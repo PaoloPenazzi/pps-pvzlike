cebb00132688e93fc807757a09f4b72d6b9b63ab
Davide Alpi <davide.alpi@studio.unibo.it>
2022-10-21 18:15:17 +0200

    feat: Detect click on card and subsequent click on cells The first click on card triggers a brief animation to give user feedback, and set the troop type to be generated (now hardcoded to Peashooter) to pending. Subsequent click on every point on the screen will remove the pending state. If subsequent click is on the grid, a position for the new turret will be calculated (center of the cell).

--- src/main/scala/view/ViewportSpace.scala
+++ src/main/scala/view/ViewportSpace.scala
@@ -1,11 +1,31 @@
 package view
 
-import model.entities.WorldSpace.{LanesLength, NumOfLanes}
+import com.badlogic.gdx.math.{GridPoint2, Rectangle, Vector2}
+import model.entities.WorldSpace.*
 
-object ViewportSpace {
+object ViewportSpace:
   def ViewportWidth = 16
   def ViewportHeight = 9
-  def HUDHeight = 4
-  def projectX(x:Float): Float = x/LanesLength*ViewportWidth
-  def projectY(y: Int): Float = y/NumOfLanes*(ViewportHeight - HUDHeight)
-}
+  def HUDHeight = 2f
+  def HUD: Rectangle = Rectangle(0, ViewportHeight - HUDHeight, ViewportWidth, HUDHeight)
+  def leftOffsetGrid = 1.4f
+  def rightOffsetGrid = 1.4f
+  def topOffsetGrid = 1
+  def bottomOffsetGrid = 0.4f
+  def gridWidth: Float = ViewportWidth - leftOffsetGrid - rightOffsetGrid
+  def gridHeight: Float = ViewportHeight - HUDHeight - bottomOffsetGrid - topOffsetGrid
+  def grid: Rectangle = Rectangle(leftOffsetGrid, bottomOffsetGrid, gridWidth, gridHeight)
+  val cells: Seq[Rectangle] =
+    val cellWidth = gridWidth/(LanesLength/CellLength)
+    val cellHeight = gridHeight/NumOfLanes
+    for
+      i <- 0 until LanesLength.toInt / CellLength.toInt
+      j <- 0 until NumOfLanes
+    yield
+      Rectangle(i*cellWidth + leftOffsetGrid, j*cellHeight + bottomOffsetGrid, cellWidth, cellHeight)
+  def projectX(x:Float): Float = (x + leftOffsetGrid * (LanesLength/gridWidth)) / LanesLength * gridWidth
+  def projectY(y: Int): Float = (y + bottomOffsetGrid * (NumOfLanes/gridHeight)) / NumOfLanes * gridHeight
+  def unprojectY(y: Float): Int = (y - bottomOffsetGrid * (NumOfLanes/gridHeight)).toInt
+  def unprojectX(x: Float): Float = (x - leftOffsetGrid) * (LanesLength/gridWidth)
+  def turretCoordinates(point: Vector2): Option[Position] =
+    cells.find(_.contains(point)).map(cell => Position(unprojectY(cell.y), unprojectX(cell.x + cell.width/2)))
