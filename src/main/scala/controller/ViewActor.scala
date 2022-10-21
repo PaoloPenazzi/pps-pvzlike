0ae0fc0174eb5abc8e4fe75cc1adb543d32fa02b
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-21 19:48:26 +0200

    feat: viewactor receive the troop spawn message. The viewactor receive a message of place troop from its view and then the actor forward the message to gameloop

--- src/main/scala/controller/ViewActor.scala
+++ src/main/scala/controller/ViewActor.scala
@@ -3,20 +3,25 @@ package controller
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import controller.Command
-import controller.GameLoopActor.GameLoopCommands.GameLoopCommand
+import controller.GameLoopActor.GameLoopCommands.{GameLoopCommand, PlaceTurret}
 import model.actors.ModelMessage
 import model.common.Utilities.MetaData
-import model.entities.Entity
+import model.entities.{Entity, Turret}
 import view.View.EntityRenderer
 
 trait ViewMessage
-case class Render(entities: List[Entity], replyTo: ActorRef[GameLoopCommand], metaData: MetaData) extends ViewMessage
+case class Render(entities: List[Entity], replyTo: ActorRef[Command], metaData: MetaData) extends ViewMessage
 
 object ViewActor:
+  var gameLoopActor: Option[ActorRef[Command]] = None
+  def sendPlaceTurret(turret: Turret): Unit =
+    gameLoopActor.foreach(_ ! PlaceTurret(turret))
+
   def apply(renderer: EntityRenderer): Behavior[ViewMessage] =
     Behaviors.receive((ctx, msg) => {
       msg match
-        case Render(list, _, _) =>
+        case Render(list, replyTo, metaData) =>
           renderer.renderEntities(list)
+          gameLoopActor = Some(replyTo)
           Behaviors.same
     })
