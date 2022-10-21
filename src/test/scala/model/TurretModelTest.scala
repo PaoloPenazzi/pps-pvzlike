d7b1e4f5b357711a192ee8704119e8dd1b9acf97
paolo <paolo.penazzi@studio.unibo.it>
2022-10-21 21:55:41 +0200

    test: change dummyEntities type to Troop

--- src/test/scala/model/TurretModelTest.scala
+++ src/test/scala/model/TurretModelTest.scala
@@ -1,4 +1,3 @@
-
 package model
 
 import model.entities.*
@@ -13,24 +12,24 @@ import model.entities.TroopState.{Attacking, Dead}
 import scala.concurrent.duration.FiniteDuration
 
 class TurretModelTest extends AnyFlatSpec with should.Matchers:
-  val testTurret: Turret = PeaShooter((1, LanesLength / 2))
-  val lowHealthTurret: Turret = PeaShooter((1, LanesLength), 25)
-  val dummyTurret1: Turret = PeaShooter((2, LanesLength))
-  val dummyZombie1: Enemy = Zombie((1, LanesLength))
-  val dummyZombie2: Enemy = Zombie((2, LanesLength))
+  val turret: Troop = PeaShooter((1, 50))
+  val lowHealthTurret: Troop = PeaShooter((1, LanesLength), 25)
+  val dummyZombie1: Troop = Zombie((1, 60))
+  val dummyTurret2: Troop = PeaShooter((2, LanesLength))
+  val dummyZombie2: Troop = Zombie((2, LanesLength))
   val dummyBullet: Bullet = PeaBullet(0,0)
 
   "A turret" should "enter attacking state if interests list is not empty" in {
-      testTurret.update(FiniteDuration(16, "milliseconds"), List(dummyZombie1)).state shouldBe Attacking
+      turret.update(FiniteDuration(16, "milliseconds"), List(dummyZombie1)).state shouldBe Attacking
   }
   "A turret" should "filter the interesting entities" in {
-    val entities: List[Entity] = List(dummyTurret1, dummyZombie1, dummyZombie2)
-    val entitiesFiltered = entities.filter(testTurret.isInterestedIn)
+    val entities: List[Entity] = List(dummyTurret2, dummyZombie1, dummyZombie2)
+    val entitiesFiltered = entities.filter(turret.isInterestedIn)
     assertResult(entitiesFiltered)(List(dummyZombie1))
   }
-  "A turret" should "lose HPs after getting hitted" in {
-    val updatedTurret = testTurret collideWith dummyBullet
-    updatedTurret.life should be < testTurret.life
+  "A turret" should "lose HPs after getting hit" in {
+    val updatedTurret = turret collideWith dummyBullet
+    updatedTurret.life should be < turret.life
   }
   "A turret" should "die if reaches 0 HP" in {
     val updatedTurret = lowHealthTurret collideWith dummyBullet
