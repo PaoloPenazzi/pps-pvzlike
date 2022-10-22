f795bd6e2b7b3fb4e42d817d14cb63f78865db55
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-22 16:34:35 +0200

    fix: bullet collided also with passed zombies. Add another check on checkCollisionWith method

--- src/main/scala/model/entities/Bullet.scala
+++ src/main/scala/model/entities/Bullet.scala
@@ -9,7 +9,7 @@ trait Bullet extends MovingAbility with Entity:
   def damage: Int = damages(this)
   def shouldDisappearAfterHitting(entity: Entity): Boolean = true
   def checkCollisionWith(entity: Entity): Boolean =
-    entity.position.x - entity.width <= position.x + width
+    entity.position.x <= position.x && position.x <= entity.position.x + entity.width
 
 class PeaBullet(override val position: Position) extends Bullet:
   override def velocity: Float = 0.1
@@ -21,3 +21,15 @@ class PeaBullet(override val position: Position) extends Bullet:
 
   override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Bullet =
     PeaBullet(updatePosition(elapsedTime))
+
+class Paw(override val position: Position) extends Bullet:
+  override def velocity: Float = -0.1
+
+  override def checkCollisionWith(entity: Entity): Boolean =
+    entity match
+      case _: Enemy => false
+      case _: Turret => super.checkCollisionWith(entity)
+  
+  override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Bullet =
+    Paw(updatePosition(elapsedTime))
+
