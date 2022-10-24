64d1e8d68cb511a8688195865770f221867203e7
paolo <paolo.penazzi@studio.unibo.it>
2022-10-24 11:52:49 +0200

    docs: add documentation to Peashooter turret

--- src/main/scala/model/entities/Turret.scala
+++ src/main/scala/model/entities/Turret.scala
@@ -28,6 +28,13 @@ abstract class Turret(override val position: Position,
 
   override def bullet: Bullet = bullets(this)
 
+/**
+ * The Peashooter is the base turret of the game.
+ *
+ * @param position the position in which the turret is placed.
+ * @param life the life that the turret currently has.
+ * @param state the state of the turret.
+ */
 case class PeaShooter(override val position: Position,
                       override val life: Int = peashooterDefaultLife,
                       override val state: TroopState = defaultTurretState) extends Turret(position, life, state):
