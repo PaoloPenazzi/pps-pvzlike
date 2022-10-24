5e5937a26ae1eff18bd9cdce3b907ab5a58e9131
paolo <paolo.penazzi@studio.unibo.it>
2022-10-24 17:25:01 +0200

    test: test wallnut model

--- src/test/scala/model/PlantModelTest.scala
+++ src/test/scala/model/PlantModelTest.scala
@@ -15,27 +15,34 @@ class PlantModelTest extends AnyFlatSpec with should.Matchers:
   val testingLane = 1
   val otherLane = 2
   val peashooter: Troop = PeaShooter((testingLane, 10))
+  val wallnut: Troop = Wallnut((testingLane, 10))
   val zombieInTheSameLane: Troop = Zombie((testingLane, 50))
   val zombieOutOfRange: Troop = Zombie((testingLane, 100))
-  val lowHealthPeashooter: Troop = PeaShooter((1, LanesLength)) withLife 25
   val dummyPlant: Troop = PeaShooter((otherLane, 50))
   val dummyZombie: Troop = Zombie((otherLane, 50))
   val dummyBullet: Bullet = Paw(0,0)
 
-  "A peashooter" should "be in Idle state if it can't attack any enemy" in {
+  "A Peashooter" should "be in Idle state if it can't attack any enemy" in {
     peashooter.update(FiniteDuration(16, "milliseconds"), List()).state shouldBe Idle
   }
-  "A peashooter" should "enter attacking state if it can attack an enemy" in {
+  "A Peashooter" should "enter attacking state if it can attack an enemy" in {
     peashooter.update(FiniteDuration(16, "milliseconds"), List(zombieInTheSameLane)).state shouldBe Attacking
   }
-  "A peashooter" should "filter the interesting entities" in {
-    val entitiesFiltered = List(dummyPlant, zombieInTheSameLane, dummyZombie).filter(peashooter.isInterestedIn)
-    assertResult(entitiesFiltered)(List(zombieInTheSameLane))
+  "A Peashooter" should "filter the interesting entities" in {
+    List(dummyPlant, zombieInTheSameLane, dummyZombie).filter(peashooter.isInterestedIn) shouldBe List(zombieInTheSameLane)
   }
-  "A peashooter" should "lose HPs after getting hit" in {
-    val updatedTurret = peashooter collideWith dummyBullet
-    updatedTurret.life should be < peashooter.life
+  "A Peashooter" should "lose HPs after getting hit" in {
+    (peashooter collideWith dummyBullet).life should be < peashooter.life
   }
-  "A peashooter" should "die if reaches 0 HP" in {
-    (lowHealthPeashooter collideWith dummyBullet).state shouldBe Dead
+  "A Peashooter" should "die if reaches 0 or less HP" in {
+    (peashooter withLife 15 collideWith dummyBullet).state shouldBe Dead
+  }
+  "A Wallnut" should "not have interests" in {
+    List(dummyPlant, zombieInTheSameLane, dummyZombie).filter(wallnut.isInterestedIn) shouldBe List.empty
+  }
+  "A Wallnut" should "lose HPs after getting hit" in {
+    (wallnut collideWith dummyBullet).life should be < wallnut.life
+  }
+  "A Wallnut" should "die if reaches 0 or less HP" in {
+    (wallnut withLife 20 collideWith dummyBullet).state shouldBe Dead
   }
