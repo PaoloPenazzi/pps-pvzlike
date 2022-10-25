1bf34aa2c471b655c38926eca7c0b2e4856050d2
paolo <paolo.penazzi@studio.unibo.it>
2022-10-25 16:47:19 +0200

    docs: add documentation to Bullets

--- src/main/scala/model/entities/Bullet.scala
+++ src/main/scala/model/entities/Bullet.scala
@@ -6,16 +6,39 @@ import model.entities.WorldSpace.{Position, given}
 
 import scala.concurrent.duration.FiniteDuration
 
+/**
+ * An [[Entity]] shoot by a [[Troop]].
+ */
 trait Bullet extends Entity with MovingAbility:
-  override type UpdatedEntity = Bullet
+
+  /**
+   * @return The damage the bullet does.
+   */
   def damage: Int = damages(this)
+
+ 
+  /**
+   * @param entity The entity the bullet collided with.
+   * @return True if the [[Bullet]] should disappear, false otherwise.
+   */
   def shouldDisappearAfterHitting(entity: Entity): Boolean = true
+
+  /**
+   * @param entity The entity with which the bullet has potentially collided
+   * @return True if a collision happened, false otherwise.
+   */
+  def checkCollisionWith(entity: Entity): Boolean = collideWith(entity) && isInMyLane(entity)
+
+  override type UpdatedEntity = Bullet
   override def velocity: Float = DefaultValues.velocity(this)
-  def checkCollisionWith(e: Entity): Boolean = collideWith(e) && isInMyLane(e)
   private def isInMyLane(entity: Entity): Boolean = entity.position.y == position.y
   private def collideWith(entity: Entity): Boolean =
     entity.position.x <= position.x && position.x <= entity.position.x + entity.width
 
+/**
+ * The [[Bullet]] shoot by the [[Peashooter]]. It has no strange effects on the zombies beside dealing damage.
+ * @param position The initial position of the [[Bullet]].
+ */
 class PeaBullet(override val position: Position) extends Bullet:
   override def checkCollisionWith(entity: Entity): Boolean =
     entity match
@@ -28,7 +51,10 @@ class PeaBullet(override val position: Position) extends Bullet:
   private def updatePosition(elapsedTime: FiniteDuration): Position =
     (position.y, position.x + (elapsedTime.length * velocity))
 
-
+/**
+ * The [[Bullet]] shoot by the [[Zombie]]. It has no strange effects on the plants beside dealing damage.
+ * @param position The initial position of the [[Bullet]].
+ */
 class Paw(override val position: Position) extends Bullet:
   override def velocity: Float = -0.1
 
