048719e67f659f439564a4c1311c2e55fe192b97
paolo <paolo.penazzi@studio.unibo.it>
2022-10-25 17:14:43 +0200

    refactor: create abstract class Bullet

--- src/main/scala/model/entities/Bullet.scala
+++ src/main/scala/model/entities/Bullet.scala
@@ -9,14 +9,13 @@ import scala.concurrent.duration.FiniteDuration
 /**
  * An [[Entity]] shoot by a [[Troop]].
  */
-trait Bullet extends Entity with MovingAbility:
+abstract class Bullet(position: Position) extends Entity with MovingAbility:
 
   /**
    * @return The damage the bullet does.
    */
   def damage: Int = damages(this)
 
- 
   /**
    * @param entity The entity the bullet collided with.
    * @return True if the [[Bullet]] should disappear, false otherwise.
@@ -29,17 +28,23 @@ trait Bullet extends Entity with MovingAbility:
    */
   def checkCollisionWith(entity: Entity): Boolean = collideWith(entity) && isInMyLane(entity)
 
-  override type UpdatedEntity = Bullet
-  override def velocity: Float = DefaultValues.velocity(this)
   private def isInMyLane(entity: Entity): Boolean = entity.position.y == position.y
+
   private def collideWith(entity: Entity): Boolean =
     entity.position.x <= position.x && position.x <= entity.position.x + entity.width
+
+  override type UpdatedEntity = Bullet
+
+  override def velocity: Float = DefaultValues.velocity(this)
+
+  protected def updatePosition(elapsedTime: FiniteDuration): Position =
+    (position.y, position.x + (elapsedTime.length * velocity))
 
 /**
  * The [[Bullet]] shoot by the [[Peashooter]]. It has no strange effects on the zombies beside dealing damage.
  * @param position The initial position of the [[Bullet]].
  */
-class PeaBullet(override val position: Position) extends Bullet:
+case class PeaBullet(override val position: Position) extends Bullet(position):
   override def checkCollisionWith(entity: Entity): Boolean =
     entity match
       case _: Enemy => super.checkCollisionWith(entity)
@@ -48,25 +53,14 @@ class PeaBullet(override val position: Position) extends Bullet:
   override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Bullet =
     PeaBullet(updatePosition(elapsedTime))
 
-  private def updatePosition(elapsedTime: FiniteDuration): Position =
-    (position.y, position.x + (elapsedTime.length * velocity))
-
 /**
  * The [[Bullet]] shoot by the [[Zombie]]. It has no strange effects on the plants beside dealing damage.
  * @param position The initial position of the [[Bullet]].
  */
-class Paw(override val position: Position) extends Bullet:
-  override def velocity: Float = -0.1
-
+case class Paw(override val position: Position) extends Bullet(position):
+  override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Bullet = Paw(updatePosition(elapsedTime))
   override def checkCollisionWith(entity: Entity): Boolean =
     entity match
       case _: Plant => super.checkCollisionWith(entity)
       case _ => false
-
-  override def update(elapsedTime: FiniteDuration, interests: List[Entity]): Bullet =
-    Paw(updatePosition(elapsedTime))
-
-  private def updatePosition(elapsedTime: FiniteDuration): Position =
-    (position.y, position.x + (elapsedTime.length * velocity))
-
 
