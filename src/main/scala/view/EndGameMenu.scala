6926026c30f5a1bd12cea31b2bf034438a5cf4f3
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-31 23:52:54 +0100

    feat: add the functionality to re-start the match. After the click on restart button, the game will start again.

--- src/main/scala/view/EndGameMenu.scala
+++ src/main/scala/view/EndGameMenu.scala
@@ -6,73 +6,48 @@ import com.badlogic.gdx.graphics.g2d.{BitmapFont, GlyphLayout, SpriteBatch, Text
 import com.badlogic.gdx.graphics.{Color, GL20, OrthographicCamera, g2d}
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
-import com.badlogic.gdx.scenes.scene2d.{EventListener, InputEvent, Stage}
 import com.badlogic.gdx.scenes.scene2d.ui.{Button, Label, Skin, TextButton}
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.{EventListener, InputEvent, Stage}
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.{FitViewport, Viewport}
-import com.badlogic.gdx.{Game, Gdx, ScreenAdapter}
+import com.badlogic.gdx.{Gdx, ScreenAdapter}
 import controller.RootActor
 import controller.RootActor.RootCommands.StartGame
-import ViewportSpace.*
 import model.entities.Entity
 import model.entities.WorldSpace.Position
 import view.Game
-import view.ViewportSpace.{ViewportHeight, ViewportWidth}
-
-
-object TestMain:
-  @main
-  def main(): Unit =
-    val config = Lwjgl3ApplicationConfiguration()
-    config.setTitle("Test")
-    config.setResizable(false)
-    config.setWindowedMode(960, 540)
-    Lwjgl3Application(TestGame, config)
-
-object TestGame extends Game :
-
-  val viewport: Viewport = FitViewport(ViewportWidth.toFloat, ViewportHeight.toFloat)
-  lazy val batch: SpriteBatch = SpriteBatch()
-  lazy val font: BitmapFont = BitmapFont(Gdx.files.internal("assets/gameWindow/font.fnt"), Gdx.files.internal("assets/gameWindow/font.png"), false)
-  val endGameMenu: EndGameMenu = EndGameMenu()
-
-  override def create(): Unit =
-    setScreen(endGameMenu)
+import view.ViewportSpace.*
 
 object EndGameMenu:
-  val Framerate: Float = 60
-
   def apply() = new EndGameMenu()
 
-class EndGameMenu() extends ScreenAdapter :
-  private lazy val stage = new Stage(TestGame.viewport); //Set up a stage for the ui
-  private val camera = TestGame.viewport.getCamera
+  class EndGameMenu() extends ScreenAdapter :
+    private lazy val stage = new Stage(Game.viewport); //Set up a stage for the ui
+    private val camera = Game.viewport.getCamera
 
-  override def render(delta: Float): Unit =
-    TestGame.batch.setProjectionMatrix(camera.combined)
-    Gdx.gl.glClearColor(0, 0, 0, 1)
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    override def render(delta: Float): Unit =
+      Game.batch.setProjectionMatrix(camera.combined)
+      Gdx.gl.glClearColor(0, 0, 0, 1)
+      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-    TestGame.batch.begin()
-    TestGame.font.getData.setScale(0.05f)
-    TestGame.font.draw(TestGame.batch, "PVZ", ViewportWidth.toFloat / 2.3f, ViewportHeight - HUDHeight / 2)
-    TestGame.batch.end()
-    stage.draw(); //Draw the ui
-    stage.act(delta)
+      Game.batch.begin()
+      Game.font.getData.setScale(0.05f)
+      Game.font.draw(Game.batch, "PVZ", ViewportWidth.toFloat / 2.3f, ViewportHeight - HUDHeight / 2)
+      Game.batch.end()
+      stage.draw(); //Draw the ui
+      stage.act(delta)
 
-  override def show(): Unit =
-    stage.clear()
-    val restart = SimpleButton("Restart", ViewportWidth.toFloat / 2.5f, ViewportHeight - HUDHeight / 0.25f) {
-      // TODO restart the game here
-      true
-    }
-
-    stage.addActor(restart)
-    Gdx.input.setInputProcessor(stage)
+    override def show(): Unit =
+      stage.clear()
+      val restart = SimpleButton("Restart", ViewportWidth.toFloat / 2.5f, ViewportHeight - HUDHeight / 0.25f) {
+        Game.startNewGame()
+        true
+      }
+      stage.addActor(restart)
+      Gdx.input.setInputProcessor(stage)
 
 object SimpleButton:
-
   def apply(text: String, x: Float, y: Float)(func: => Boolean): Button =
     val button = new TextButton(text, new Skin(Gdx.files.internal("assets/skin/default/uiskin.json")))
     button.setTransform(true)
