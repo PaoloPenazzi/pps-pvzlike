02c3ff96104c11f022face2f07572e7ed992a4d4
Ocsof <francesco.foschini6@studio.unibo.it>
2022-10-31 17:19:18 +0100

    test: implement WarriorZombie's tests

--- src/test/scala/model/EnemyModelTest.scala
+++ src/test/scala/model/EnemyModelTest.scala
@@ -16,6 +16,7 @@ class EnemyModelTest extends AnyFlatSpec with should.Matchers:
   private val otherLane = 2
   private val zombie: Troop = Troops.ofType[Zombie] withPosition (testingLane, 80)
   private val fastZombie: Troop = Troops.ofType[FastZombie] withPosition (testingLane, 80)
+  private val warriorZombie: Troop = Troops.ofType[WarriorZombie] withPosition (testingLane, 80)
 
   private val plantInOtherLane: Troop = Troops.ofType[PeaShooter] withPosition (otherLane, 10)
   private val plantInTheSameLane: Troop = Troops.ofType[PeaShooter] withPosition (testingLane, 10)
@@ -63,6 +64,26 @@ class EnemyModelTest extends AnyFlatSpec with should.Matchers:
   }
   "A FastZombie" should "not have interests" in {
     List(dummyZombie, plantInTheSameLane, dummyBullet) filter fastZombie.isInterestedIn shouldBe List.empty
+  }
+
+  "A WarriorZombie" should "be in Moving state if it can't attack any plant" in {
+    warriorZombie.update(FiniteDuration(16, "milliseconds"), List()).state shouldBe Moving
+  }
+  "A WarriorZombie" should "enter attacking state if it can attack an enemy" in {
+    warriorZombie.update(FiniteDuration(16, "milliseconds"), List(plantInTheSameLane)).state shouldBe Attacking
+  }
+  "A WarriorZombie" should "filter the interesting entities" in {
+    println(warriorZombie.isInterestedIn(plantInRange))
+    List(dummyPlant, plantInTheSameLane, dummyZombie, dummyBullet, plantInRange) filter warriorZombie.isInterestedIn shouldBe List(plantInRange)
+  }
+  "A WarriorZombie" should "lose HPs after getting hit" in {
+    (warriorZombie collideWith dummyBullet).life should be < warriorZombie.life
+  }
+  "A WarriorZombie" should "die if reaches 0 or less HP" in {
+    (warriorZombie withLife 25 collideWith dummyBullet).state shouldBe Dead
+  }
+  "A WarriorZombie" should "not have interests" in {
+    List(dummyZombie, plantInTheSameLane, dummyBullet) filter warriorZombie.isInterestedIn shouldBe List.empty
   }
 
 
