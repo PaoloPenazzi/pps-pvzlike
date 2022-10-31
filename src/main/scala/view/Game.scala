3e3bad0bf1e0c24bc8ba172485e8f8b98c0852db
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-10-31 23:44:25 +0100

    feat: now the game has a method to start a new game. Each time it restarts the system, then it shutdown the previous system

--- src/main/scala/view/Game.scala
+++ src/main/scala/view/Game.scala
@@ -6,20 +6,26 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.{Color, GL20, OrthographicCamera, Texture}
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.{FitViewport, Viewport}
-import com.badlogic.gdx.{ApplicationAdapter, Game, Gdx}
+import com.badlogic.gdx.{ApplicationAdapter, Game, Gdx, Screen, ScreenAdapter}
 import controller.RootActor
 import controller.RootActor.RootCommands.StartGame
 import ViewportSpace.*
-
-import scala.language.implicitConversions
-
+import controller.Command
 
 object Game extends com.badlogic.gdx.Game:
-  val viewport: Viewport = FitViewport(ViewportWidth,ViewportHeight)
-  val gameScreen: GameScreen = GameScreen()
-  val mainMenuScreen: MainMenuScreen = MainMenuScreen(gameScreen)
+  val viewport: Viewport = FitViewport(ViewportWidth.toFloat, ViewportHeight.toFloat)
+  var mainMenuScreen: ScreenAdapter = EndGameMenu()
+  var actorSystem: Option[ActorSystem[Command]] = None
   lazy val batch: SpriteBatch = SpriteBatch()
   lazy val font: BitmapFont = BitmapFont(Gdx.files.internal("assets/gameWindow/font.fnt"), Gdx.files.internal("assets/gameWindow/font.png"), false)
+
+  def startNewGame(): Unit =
+    val gameScreen = GameScreen()
+    setScreen(gameScreen)
+    actorSystem.foreach(_.terminate())
+    actorSystem = Some(ActorSystem(RootActor(), "launcher"))
+    actorSystem.foreach(_ ! StartGame(gameScreen))
+
   override def create(): Unit =
     setScreen(mainMenuScreen)
     
