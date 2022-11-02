2bc404c0c485bac4f634ee67ed340311de300652
Ocsof <francesco.foschini6@studio.unibo.it>
2022-11-02 11:01:36 +0100

    refactor: remove useless import

--- src/main/scala/view/MainMenuScreen.scala
+++ src/main/scala/view/MainMenuScreen.scala
@@ -6,6 +6,7 @@ import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.Stage
 import controller.RootActor
 import controller.RootActor.RootCommands.StartGame
 
@@ -13,27 +14,25 @@ object MainMenuScreen:
   def apply() = new MainMenuScreen()
 
   class MainMenuScreen() extends ScreenAdapter:
-    lazy val font: BitmapFont = BitmapFont()
-  
     private lazy val camera: OrthographicCamera = OrthographicCamera()
-  
+
     override def render(delta: Float): Unit =
       ScreenUtils.clear(0, 0, 0.2f, 1)
       Gdx.gl.glClearColor(0, 0, 0, 1)
       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-  
+
       camera.update()
-  
+
       Game.batch.setProjectionMatrix(camera.combined)
-  
+
       Game.batch.begin()
-      font.draw(Game.batch, "Welcome to Plant vs Zombie!!! ", 100, 150)
-      font.draw(Game.batch, "Tap anywhere to begin!", 100, 100)
+      Game.font.draw(Game.batch, "Welcome to Plant vs Zombie!!! ", 100, 150)
+      Game.font.draw(Game.batch, "Tap anywhere to begin!", 100, 100)
       Game.batch.end()
-  
+
       if Gdx.input.isTouched() then
         Game.startNewGame()
         dispose()
-  
+
     override def show(): Unit =
       camera.setToOrtho(false, 800, 480)
