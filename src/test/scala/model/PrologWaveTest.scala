848f9c6216f61161005accb266a24d499d2b5e05
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-03 16:53:58 +0100

    test: create first val for testing prologwaves

---
+++ src/test/scala/model/PrologWaveTest.scala
@@ -0,0 +1,14 @@
+package model
+
+import model.waves.PrologWaveManager.*
+import model.waves.PrologWaveManager.PrologEngines.PrologEngine
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class PrologWaveTest extends AnyFlatSpec with should.Matchers:
+  val pathTheory = "prolog/waves.pl"
+
+  val stringTheory: String = PrologTheory.getStringTheory(pathTheory)
+  val engine: PrologEngine = PrologEngine(PrologTheory.getTheory(stringTheory))
+
+
