2a5b1f95e10f2d33f439dc95b21ac3c2a2135991
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-04 09:41:22 +0100

    test: add test for new given conversion data type

--- src/test/scala/model/GameDataTest.scala
+++ src/test/scala/model/GameDataTest.scala
@@ -13,40 +13,29 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class GameDataTest extends AnyFlatSpec with BeforeAndAfter with Matchers :
-  var seedActor: TestInbox[ModelMessage] = _
-  var zombieActor: TestInbox[ModelMessage] = _
-  var plantActor: TestInbox[ModelMessage] = _
 
-  var bullet: GameEntity[ModelMessage, Entity] = _
-  var zombie: GameEntity[ModelMessage, Entity] = _
-  var shooter: GameEntity[ModelMessage, Entity] = _
+  val seedActor: TestInbox[ModelMessage] = TestInbox[ModelMessage]("seed")
+  val zombieActor: TestInbox[ModelMessage] = TestInbox[ModelMessage]("zombie")
+  val plantActor: TestInbox[ModelMessage] = TestInbox[ModelMessage]("plant")
 
-  var seq: GameSeq[ModelMessage] = _
+  val bullet: GameEntity[ModelMessage, Entity] = GameEntity(seedActor.ref, PeaBullet(1, LanesLength))
+  val zombie: GameEntity[ModelMessage, Entity] = GameEntity(zombieActor.ref, BasicZombie((1, LanesLength)))
+  val shooter: GameEntity[ModelMessage, Entity] = GameEntity(plantActor.ref, PeaShooter(Position(1, LanesLength / 2)))
 
-  before {
-    seedActor = TestInbox[ModelMessage]("seed")
-    zombieActor = TestInbox[ModelMessage]("zombie")
-    plantActor = TestInbox[ModelMessage]("plant")
-
-    bullet = GameEntity(seedActor.ref, PeaBullet(1, LanesLength))
-    zombie = GameEntity(zombieActor.ref, BasicZombie((1, LanesLength)))
-    shooter = GameEntity(plantActor.ref, PeaShooter(Position(1, LanesLength / 2)))
-
-    seq = GameSeq(List(bullet, zombie, shooter))
-  }
+  val seq: GameSeq[ModelMessage] = GameSeq(List(bullet, zombie, shooter))
 
   import GameData.*
 
   "a seq" should "make seq of turret" in {
     assertResult(seq.ofType[Plant])(List(shooter))
   }
-//
-//  "a seq" should "make seq of bullet" in {
-//    assertResult(seq.ofType[Bullet])(List(bullet))
-//  }
-//
-//  "a seq" should "make seq of enemy" in {
-//    assertResult(seq.ofType[Enemy])(List(zombie))
-//  }
+
+  "a seq" should "make seq of zombie" in {
+    assertResult(seq.ofType[Zombie])(List(zombie))
+  }
+
+  "a seq" should "make seq of bullet" in {
+    assertResult(seq.ofType[Bullet])(List(bullet))
+  }
 
 
