6c66feb66adad11fe320688fe8c900d0053ac45d
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-04 11:29:15 +0100

    refactor: delete useless generic type in GameSeq

--- src/main/scala/model/GameData.scala
+++ src/main/scala/model/GameData.scala
@@ -1,34 +1,33 @@
 package model
 
 import akka.actor.typed.ActorRef
-// import model.GameData.GameSeq.GameSeqBuilder
 import model.GameData.GameSelector.GameSelectorBuilder
 import model.actors.ModelMessage
 import model.entities.{Bullet, Entity, PeaShooter, Plant, Zombie}
 
 object GameData :
 
-  case class GameEntity[T, E <: Entity](ref: ActorRef[T], entity: E)
+  case class GameEntity[E <: Entity](ref: ActorRef[ModelMessage], entity: E)
 
-  case class GameSeq[T](seq: Seq[GameEntity[T, Entity]]):
-    def ofType[A <: Entity](using select: GameSelectorBuilder[T, A]): Seq[GameEntity[T, A]] =
+  case class GameSeq(seq: Seq[GameEntity[Entity]]):
+    def ofType[A <: Entity](using select: GameSelectorBuilder[A]): Seq[GameEntity[A]] =
       seq.collect(select.of)
 
   object GameSelector:
-    trait GameSelectorBuilder[T, E <: Entity]:
-      def of: PartialFunction[GameEntity[T, Entity], GameEntity[T, E]]
+    trait GameSelectorBuilder[E <: Entity]:
+      def of: PartialFunction[GameEntity[Entity], GameEntity[E]]
 
-    given GameSelectorBuilder[ModelMessage, Plant] with
-      override def of: PartialFunction[GameEntity[ModelMessage, Entity], GameEntity[ModelMessage, Plant]] =
+    given GameSelectorBuilder[Plant] with
+      override def of: PartialFunction[GameEntity[Entity], GameEntity[Plant]] =
          { case e if e.entity.isInstanceOf[Plant] => GameEntity(e.ref, e.entity.asInstanceOf[Plant]) }
 
-    given GameSelectorBuilder[ModelMessage, Zombie] with
-      override def of: PartialFunction[GameEntity[ModelMessage, Entity], GameEntity[ModelMessage, Zombie]] = {
+    given GameSelectorBuilder[Zombie] with
+      override def of: PartialFunction[GameEntity[Entity], GameEntity[Zombie]] = {
         case e if e.entity.isInstanceOf[Zombie] => GameEntity(e.ref, e.entity.asInstanceOf[Zombie])
       }
 
-    given GameSelectorBuilder[ModelMessage, Bullet] with
-      override def of: PartialFunction[GameEntity[ModelMessage, Entity], GameEntity[ModelMessage, Bullet]] = {
+    given GameSelectorBuilder[Bullet] with
+      override def of: PartialFunction[GameEntity[Entity], GameEntity[Bullet]] = {
         case e if e.entity.isInstanceOf[Bullet] => GameEntity(e.ref, e.entity.asInstanceOf[Bullet])
       }
 
