dcbf511150a5c404673d2823c2fd59b9c8ac9942
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-04 16:46:02 +0100

    feat: add object used to create a real-solution from the prolog solution. Now, from a term in input, we can create a list of zombie through the prolog engine and our theory

--- src/main/scala/model/waves/PrologWaveManager.scala
+++ src/main/scala/model/waves/PrologWaveManager.scala
@@ -3,6 +3,7 @@ package model.waves
 import alice.tuprolog.{Engine, Number, Prolog, SolveInfo, Struct, Term, Theory}
 import model.entities.{BasicZombie, FastZombie, Troops, WarriorZombie, Zombie}
 
+import java.util.Scanner
 import scala.io.Source
 
 object PrologWaveManager:
@@ -27,7 +28,22 @@ object PrologWaveManager:
     def getStringTheory(resourcePath: String): String = Source.fromResource(resourcePath).mkString
 
     def getTheory(stringTheory: String): Theory = Theory.parseWithStandardOperators(stringTheory)
-    
+
   object WaveTerm:
-    
+
     def queryToTerm(query: String): Term = Term.createTerm(query)
+
+  object PrologSolution:
+
+    def waveFromPrologSolution(solution: SolveInfo): List[Zombie] =
+      waveFromTerm(solution.getTerm("L"))
+
+    def waveFromTerm(term: Term): List[Zombie] =
+      term.toString.replaceAll("\\[|\\]", "")
+                   .split(",")
+                   .foldRight(List.empty[Int])((e, acc) => acc :+ e.toInt)
+                   .map(e => e match
+                     case 1 => BasicZombie()
+                     case 2 => FastZombie()
+                     case 3 => WarriorZombie())
+
