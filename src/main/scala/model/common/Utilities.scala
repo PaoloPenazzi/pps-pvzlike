9e7dffd82b3f10a51ac9b60a91968020077c2ef1
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-05 17:17:38 +0100

    refactor: rename velocity in speed

--- src/main/scala/model/common/Utilities.scala
+++ src/main/scala/model/common/Utilities.scala
@@ -7,17 +7,17 @@ import scala.concurrent.duration.FiniteDuration
 
 object Utilities:
 
-  enum Velocity(val speed: FiniteDuration):
-    case Slow extends Velocity(FiniteDuration(32, "milliseconds"))
-    case Normal extends Velocity(FiniteDuration(16, "milliseconds"))
-    case Fast extends Velocity(FiniteDuration(8, "milliseconds"))
+  enum Speed(val speed: FiniteDuration):
+    case Slow extends Speed(FiniteDuration(32, "milliseconds"))
+    case Normal extends Speed(FiniteDuration(16, "milliseconds"))
+    case Fast extends Speed(FiniteDuration(8, "milliseconds"))
 
   enum Sun(val value: Int):
     case Small extends Sun(15)
     case Normal extends Sun(50)
     case Big extends Sun(100)
 
-  case class MetaData(sun: Int = 0, velocity: Velocity = Velocity.Normal):
+  case class MetaData(sun: Int = 0, velocity: Speed = Speed.Normal):
     @targetName("sum")
     def +(quantity: Int): MetaData = MetaData(quantity + sun, velocity)
 
@@ -25,5 +25,5 @@ object Utilities:
     def -(quantity: Int): MetaData = MetaData(sun - quantity, velocity)
 
     @targetName("change velocity")
-    def >>>(newVelocity: Velocity): MetaData = MetaData(sun, newVelocity)
+    def >>>(newVelocity: Speed): MetaData = MetaData(sun, newVelocity)
 
