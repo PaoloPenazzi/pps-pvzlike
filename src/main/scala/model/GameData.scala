e3b18486c0fd3ca122ecce1477799c4e51481d20
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-05 17:09:52 +0100

    refactor: now GameSeq is an extension of Seq

--- src/main/scala/model/GameData.scala
+++ src/main/scala/model/GameData.scala
@@ -3,7 +3,7 @@ package model
 import akka.actor.typed.ActorRef
 import model.GameData.GameSelector.GameSelectorBuilder
 import model.actors.ModelMessage
-import model.entities.{Bullet, Entity, PeaShooter, Plant, Zombie}
+import model.entities.{Bullet, Entity, PeaShooter, Plant, Troop, Zombie}
 
 object GameData :
 
@@ -12,24 +12,19 @@ object GameData :
   case class GameSeq(seq: Seq[GameEntity[Entity]]):
     def ofType[A <: Entity](using select: GameSelectorBuilder[A]): Seq[GameEntity[A]] =
       seq.collect(select.of)
-
+  given Conversion[Seq[GameEntity[Entity]], GameSeq] = GameSeq(_)
+    
   object GameSelector:
     trait GameSelectorBuilder[E <: Entity]:
       def of: PartialFunction[GameEntity[Entity], GameEntity[E]]
 
-    given GameSelectorBuilder[Plant] with
-      override def of: PartialFunction[GameEntity[Entity], GameEntity[Plant]] =
-         { case e if e.entity.isInstanceOf[Plant] => GameEntity(e.ref, e.entity.asInstanceOf[Plant]) }
-
-    given GameSelectorBuilder[Zombie] with
-      override def of: PartialFunction[GameEntity[Entity], GameEntity[Zombie]] = {
-        case e if e.entity.isInstanceOf[Zombie] => GameEntity(e.ref, e.entity.asInstanceOf[Zombie])
-      }
+    given GameSelectorBuilder[Troop] with
+      override def of: PartialFunction[GameEntity[Entity], GameEntity[Troop]] =
+        { case e if e.entity.isInstanceOf[Troop] => GameEntity(e.ref, e.entity.asInstanceOf[Troop]) }
 
     given GameSelectorBuilder[Bullet] with
-      override def of: PartialFunction[GameEntity[Entity], GameEntity[Bullet]] = {
-        case e if e.entity.isInstanceOf[Bullet] => GameEntity(e.ref, e.entity.asInstanceOf[Bullet])
-      }
+      override def of: PartialFunction[GameEntity[Entity], GameEntity[Bullet]] =
+        { case e if e.entity.isInstanceOf[Bullet] => GameEntity(e.ref, e.entity.asInstanceOf[Bullet]) }
 
 
 
