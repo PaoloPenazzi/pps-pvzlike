1634753b28435c6826ead8bf9ad7ad264fb235de
Davide Alpi <davide.alpi@studio.unibo.it>
2022-11-08 19:00:11 +0100

    refactor: use ScalaGdx API to implement GameScreen behavior

--- src/main/scala/view/GameScreen.scala
+++ src/main/scala/view/GameScreen.scala
@@ -1,122 +1,59 @@
 package view
 
-import com.badlogic.gdx.Input.Buttons
-import com.badlogic.gdx.graphics.g2d.{BitmapFont, SpriteBatch, TextureRegion}
-import com.badlogic.gdx.graphics.{GL20, Texture}
-import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.physics.box2d.World
-import com.badlogic.gdx.scenes.scene2d.{InputEvent, Stage}
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
-import com.badlogic.gdx.scenes.scene2d.utils.{ClickListener, TextureRegionDrawable}
-import com.badlogic.gdx.scenes.scene2d.actions.Actions
-import com.badlogic.gdx.{Gdx, ScreenAdapter}
+import com.badlogic.gdx.scenes.scene2d.Actor
 import view.View.EntityRenderer
 import view.ViewportSpace.*
-import view.Sprites.{height, spriteName, width}
+import view.Sprites.{cardName, height, spriteName, width}
 import model.entities.{CherryBomb, Entity, PeaBullet, Shooter, SnowBullet, Troop, Troops, Wallnut}
 import model.common.Utilities.MetaData
 import controller.ViewActor.sendPlacePlant
-import ScalaGDX.*
-import ScalaGDX.given
-
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.viewport.Viewport
+import view.ScalaGDX.{Drawable, Writable}
+import ScalaGDX.{PulsingImageButton, given}
+import ScalaGDX.Utils.texture
+import view.ScalaGDX.Screen.ScreenBehavior
 import scala.language.implicitConversions
 
 
-object GameScreen:
-  def apply() = new GameScreen()
-
-class GameScreen() extends ScreenAdapter with EntityRenderer :
-  private val camera = Game.viewport.getCamera
+class GameScreen extends ScreenBehavior with EntityRenderer :
+  private var pendingPlant: Option[Troop] = None
   private var entities: List[Entity] = List.empty
-
-  private lazy val stage = new Stage(Game.viewport)
-  var pendingPlant: Option[Troop] = None
-
-  lazy val background: Texture = Texture(Gdx.files.classpath("assets/background/day.png"))
-  lazy val font: BitmapFont = BitmapFont(Gdx.files.internal("assets/gameWindow/font.fnt"), Gdx.files.internal("assets/gameWindow/font.png"), false)
-  lazy val batch: SpriteBatch = SpriteBatch()
-  lazy val gamingWindowNumberOfSun: Texture = Texture(Gdx.files.classpath("assets/gameWindow/numberOfSun.png"))
-
-
   private var metaData: MetaData = MetaData()
 
-  override def render(delta: Float): Unit =
-    batch.setProjectionMatrix(camera.combined)
-    Gdx.gl.glClearColor(0, 0, 0, 1)
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+  override def drawables: Seq[Drawable] =
+    def drawableEntities: Seq[Drawable] =
+      // The entities on lower lanes are rendered in a more foreground position, to better represent an isometric point of view
+      given Ordering[Entity] = (e1, e2) => e2.position.y - e1.position.y
+      entities.sorted.map(e => Drawable(spriteName(e), projectX(e.position.x), projectY(e.position.y), width(e), height(e)))
 
-    batch.begin()
-    batch.draw(background, -3, 0, 25, ViewportHeight - HUDHeight)
-    font.getData.setScale(.05f)
-    font.draw(batch, metaData.sun.toString, 12, 7.5f)
-    entities.foreach(e => batch.draw(texture(e), projectX(e.position.x), projectY(e.position.y), width(e), height(e)))
-    batch.end()
-    stage.draw(); //Draw the ui
-    stage.act(delta)
+    Drawable("assets/background/day.png", -3, 0, 25, ViewportHeight - HUDHeight)
+      +: drawableEntities
 
+  override def writables: Seq[Writable] =
+    Seq(Writable(metaData.sun.toString, 12f, 7.5f, 0.05f))
 
-  override def show(): Unit =
-    stage.clear()
-    val peashooterCard: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/peashooter-card.png"))
-    val wallnutCard: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/wallnut-card.png"))
-    val cherryBombCard: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/cherrybomb-card.png"))
-    val snowshooterCard: Texture = new Texture(Gdx.files.classpath("assets/gameWindow/snowshooter-card.png"))
-    createButtonFromImage(peashooterCard, 0, ViewportHeight - HUDHeight, 1.5f, 1.2f)
-    createButtonFromImage(wallnutCard, 1.5f, ViewportHeight - HUDHeight, 1.5f, 1.2f)
-    createButtonFromImage(cherryBombCard, 3.0f, ViewportHeight - HUDHeight, 1.5f, 1.2f)
-    createButtonFromImage(snowshooterCard, 4.5f, ViewportHeight - HUDHeight, 1.5f, 1.2f)
-    Gdx.input.setInputProcessor(stage)
-    stage.onTouchDown( pos =>
-      for
-        plant <- pendingPlant
-        plantCoordinates <- plantCoordinates(pos)
-      do
-        sendPlacePlant(plant withPosition plantCoordinates)
-      if pos.y < HUD.y then
-        pendingPlant = None
-    )
+  override def actors: Seq[Actor] =
+    val troops = Seq(Troops.shooterOf[PeaBullet], Troops.ofType[Wallnut], Troops.ofType[CherryBomb], Troops.shooterOf[SnowBullet])
 
-  private def createButtonFromImage(texture: Texture, x: Float, y: Float, width: Float, height: Float): ImageButton =
-    val textureRegionDrawable = new TextureRegion(texture)
-    val button = new ImageButton(new TextureRegionDrawable(textureRegionDrawable))
-    button.setBounds(x, y, width, height)
-    button.setTransform(true)
-    button.onTouchDown(_ =>
-        button.clearActions()
-        button.setScale(1.2f)
-        texture.toString match
-          case s if s.matches("""\S*peashooter\S*""") => pendingPlant = Option(Troops.shooterOf[PeaBullet])
-          case s if s.matches("""\S*snowshooter\S*""") => pendingPlant = Option(Troops.shooterOf[SnowBullet])
-          case s if s.matches("""\S*wallnut\S*""") => pendingPlant = Option(Troops.ofType[Wallnut])
-          case s if s.matches("""\S*cherrybomb\S*""") => pendingPlant = Option(Troops.ofType[CherryBomb])
-          case _ => pendingPlant = None
-    )
-    button.onTouchUp(() =>
-          button.addAction(Actions.scaleTo(1f, 1f, 0.5f))
-    )
-    stage.addActor(button)
-    button
+    for
+      (troop, x) <- troops zip (0 to troops.size).map(_ * CardWidth)
+    yield
+      val button: Actor = PulsingImageButton(texture(cardName(troop)), x, ViewportHeight - HUDHeight, CardWidth, CardWidth)
+      button.onTouchDown(_ => pendingPlant = Some(troop))
+      button
 
+  override def onScreenTouch: Vector2 => Unit = pos =>
+    for
+      plant <- pendingPlant
+      plantCoordinates <- plantCoordinates(pos)
+    do
+      sendPlacePlant(plant withPosition plantCoordinates)
+    if pos.y < HUD.y then
+      pendingPlant = None
 
-  override def resize(width: Int, height: Int): Unit =
-    Game.viewport.update(width, height)
-    camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0)
-    camera.update()
+  override def viewport: Viewport = Game.viewport
 
   def renderEntities(entities: List[Entity]): Unit = this.entities = entities
 
   def renderMetadata(metaData: MetaData): Unit = this.metaData = metaData
-
-  val texture: Entity => Texture = memoizedTexture
-
-  def memoizedTexture: Entity => Texture =
-    def texture(entity: Entity): Texture = new Texture(Gdx.files.classpath("assets/" + spriteName(entity)))
-
-    val cache = collection.mutable.Map.empty[String, Texture]
-
-    entity =>
-      val key = spriteName(entity)
-      cache.getOrElse(key, {
-        cache.update(key, texture(entity))
-        cache(key)
-      })
