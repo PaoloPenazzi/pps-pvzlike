fe141a56d8e3733dfffdc8d5daa44e0caaf58ed1
Ocsof <francesco.foschini6@studio.unibo.it>
2022-11-08 17:39:24 +0100

    test: a bullet should disappear after hitting

--- src/test/scala/model/BulletModelTest.scala
+++ src/test/scala/model/BulletModelTest.scala
@@ -1,19 +1,28 @@
 package model
 
-import model.entities.*
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
-import WorldSpace.{Position, given}
+import model.entities
+import model.entities.WorldSpace.Position
+import model.entities.{Bullets, PawBullet, PeaBullet, Troops}
 
-import scala.language.implicitConversions
+import scala.language.{implicitConversions, postfixOps}
 import scala.concurrent.duration.FiniteDuration
 
 class BulletModelTest extends AnyFlatSpec with should.Matchers:
-  "A bullet" should "update his position" in {
-    val bullet: PeaBullet = PeaBullet((10, 1))
+
+  val bullet = Bullets.ofType[PawBullet]
+  val plant = Troops.shooterOf[PeaBullet]
+
+  "A Bullet" should "update his position" in {
     val elapsedTime = FiniteDuration(32, "milliseconds")
     val expectedResult: Position = (bullet.position.y, bullet.position.x + (elapsedTime.length * bullet.velocity))
     val updatedBullet = bullet.update(elapsedTime, List.empty)
     assertResult(expectedResult)(updatedBullet.position)
   }
+  "A Bullet" should "disappear after hitting" in {
+    assertResult(true)(bullet shouldDisappearAfterHitting plant)
+  }
+
+
