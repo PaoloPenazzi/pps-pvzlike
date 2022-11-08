32d5a85d7502d8cf5695f510a4584ecbf111e61e
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-08 17:39:06 +0100

    refactor: GameSeq methods' name refactor.

--- src/main/scala/model/GameData.scala
+++ src/main/scala/model/GameData.scala
@@ -3,38 +3,44 @@ package model
 import akka.actor.typed.ActorRef
 import model.GameData.GameSelector.GameSelectorBuilder
 import model.actors.ModelMessage
-import model.entities.{Bullet, Entity, Plant, Troop, Zombie}
+import model.entities.*
 
 import scala.annotation.targetName
 
-object GameData :
+object GameData:
+
+  given Conversion[Seq[GameEntity[Entity]], GameSeq] = GameSeq(_)
 
   case class GameEntity[E <: Entity](ref: ActorRef[ModelMessage], entity: E)
 
   case class GameSeq(seq: Seq[GameEntity[Entity]]):
-    def ofType[A <: Entity](using select: GameSelectorBuilder[A]): Seq[GameEntity[A]] =
-      seq.collect(select.of)
+    def of[A <: Entity](using select: GameSelectorBuilder[A]): Seq[GameEntity[A]] =
+      seq.collect(select.by)
+
     @targetName("delete")
     def :-(ref: ActorRef[ModelMessage]): Seq[GameEntity[Entity]] =
-      seq filter {_.ref != ref}
-  given Conversion[Seq[GameEntity[Entity]], GameSeq] = GameSeq(_)
-    
+      seq filter {
+        _.ref != ref
+      }
+
   object GameSelector:
     trait GameSelectorBuilder[E <: Entity]:
-      def of: PartialFunction[GameEntity[Entity], GameEntity[E]]
+      def by: PartialFunction[GameEntity[Entity], GameEntity[E]]
 
     given GameSelectorBuilder[Troop] with
-      override def of: PartialFunction[GameEntity[Entity], GameEntity[Troop]] =
-        { case e if e.entity.isInstanceOf[Troop] => GameEntity(e.ref, e.entity.asInstanceOf[Troop]) }
+      override def by: PartialFunction[GameEntity[Entity], GameEntity[Troop]] = {
+        case e if e.entity.isInstanceOf[Troop] => GameEntity(e.ref, e.entity.asInstanceOf[Troop])
+      }
 
     given GameSelectorBuilder[Plant] with
-      override def of: PartialFunction[GameEntity[Entity], GameEntity[Plant]] = {
+      override def by: PartialFunction[GameEntity[Entity], GameEntity[Plant]] = {
         case e if e.entity.isInstanceOf[Plant] => GameEntity(e.ref, e.entity.asInstanceOf[Plant])
       }
 
     given GameSelectorBuilder[Bullet] with
-      override def of: PartialFunction[GameEntity[Entity], GameEntity[Bullet]] =
-        { case e if e.entity.isInstanceOf[Bullet] => GameEntity(e.ref, e.entity.asInstanceOf[Bullet]) }
+      override def by: PartialFunction[GameEntity[Entity], GameEntity[Bullet]] = {
+        case e if e.entity.isInstanceOf[Bullet] => GameEntity(e.ref, e.entity.asInstanceOf[Bullet])
+      }
 
 
 
