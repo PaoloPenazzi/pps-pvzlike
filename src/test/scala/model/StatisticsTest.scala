f879203d04e1b1efccba950d0e358691741306ad
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-11-08 18:28:07 +0100

    fix: fix a bug at the game startup. Previously, the game started with a number round of 0 and this one created an error. Now the number of the rounds in the statistics, start from 1.

--- src/test/scala/model/StatisticsTest.scala
+++ src/test/scala/model/StatisticsTest.scala
@@ -18,7 +18,7 @@ class StatisticsTest extends AnyFlatSpec with Matchers:
   }
 
   "Pass a round" should "increase the number of round" in {
-    GameStatistics().increaseRound().rounds shouldBe 1
+    GameStatistics().increaseRound().rounds shouldBe 2
   }
 
   "At the end of the game" should "get only the zombies" in {
