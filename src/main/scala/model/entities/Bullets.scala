1f5073157f1052dcd2f697cd61b08d95b148889b
paolo <paolo.penazzi@studio.unibo.it>
2022-11-09 09:34:32 +0100

    chore: add missing docs

--- src/main/scala/model/entities/Bullets.scala
+++ src/main/scala/model/entities/Bullets.scala
@@ -22,7 +22,10 @@ trait Bullet extends Entity with MovingAbility :
    * @return True if the [[Bullet]] should disappear, false otherwise.
    */
   def shouldDisappearAfterHitting(entity: Entity): Boolean = true
-  
+
+  /**
+   * @return true if the bullet can hit multiple times, false otherwise.
+   */
   def hitMultipleTimes: Boolean = false
 
   /**
@@ -31,7 +34,13 @@ trait Bullet extends Entity with MovingAbility :
    */
   def checkCollisionWith(entity: Entity): Boolean = collideWith(entity) && isInMyLane(entity)
 
-  def applyDamage(troop: Troop): Troop =
+  /**
+   * Apply the damage and the effect of the [[Bullet]] on the [[Troop]].
+   *
+   * @param troop the troop hit by the bullet.
+   * @return the troop updated.
+   */
+  def applyDamageAndEffect(troop: Troop): Troop =
     troop withLife (troop.life - damage)
 
   private def isInMyLane(entity: Entity): Boolean = entity.position.y == position.y
@@ -67,9 +76,10 @@ case class PeaBullet(override val position: Position = defaultBulletPosition) ex
   override def withPosition(pos: Position): PeaBullet = copy(position = pos)
 
 case class SnowBullet(override val position: Position = defaultBulletPosition) extends PlantBullet :
-  override def applyDamage(troop: Troop): Troop =
+  override def applyDamageAndEffect(troop: Troop): Troop =
     troop match
-      case z: Zombie => super.applyDamage(z.withVelocity(slowVelocities(z)))
+      case z: Zombie => super.applyDamageAndEffect(z withVelocity slowVelocities(z))
+
   override def withPosition(pos: Position): SnowBullet = copy(position = pos)
 
 /**
