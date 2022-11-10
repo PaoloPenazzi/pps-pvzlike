82d23c10f32f10f452de51865830dedc4e466fb3
Davide Alpi <davide.alpi@studio.unibo.it>
2022-11-10 14:14:16 +0100

    fix: set window resizable to false if architecture is arm64 This is to prevent M1/M2 macbooks from crashing on resize due to lwgl3 bug

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -7,7 +7,7 @@ object Launcher :
   def main(): Unit =
     val config = Lwjgl3ApplicationConfiguration()
     config.setTitle("PVZ")
-    config.setResizable(true)
+    config.setResizable(System.getProperty("os.arch") != "aarch64")
     config.setWindowedMode(960, 540)
     Lwjgl3Application(Game, config)
 
